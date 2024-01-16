7de775b97b5bb078a9a7bdfad5977c8bbc47f87b
rlwjd31 <rlwjd31@naver.com>
2024-01-16 17:40:15 +0900

    # 🎉 [Init]: initialization project ##### Subject 50 characters ################# -> |
    # Body Message
    ######## Body 72 characters ####################################### -> |
    initial project with boilerplate contains below
    - vite
    - eslint
    - prettier
    - typescript
    - react-query
    - recoil
    - tailwind
    
    # Issue Tracker Number or URL
    
    # --- COMMIT END ---
    #   제목 첫 글자는 대문자
    #   제목에 마침표, 특수기호 X
    #   간결하고 요점만 작성
    
    # <subject> 리스트
    #   🚀feat    : 새로 추가되는 기능
    #   🚑️fix     : 버그 수정 및 단순 텍스트 수정
    #   ♻️refactor: 리팩토링
    #   🎨style   : 코드 스타일 수정(포멧팅) 코드 내용 변경 X
    #   📝docs    : 문서 추가 및 수정
    #   💡comment : 주석 변경
    #   💄design  : CSS 스타일링 수정
    #   🎉init    : 프로젝트 초기 설정
    #   🚚file    : 파일 및 폴더 수정, 이동, 삭제
    #   🐛bug     : 버그 reporting할 때, @FIXME 주석 태그 삽입
    #   🔨chore   : 기타 변경사항
    #             - 빌드 스크립트 수정
    #             - asset 수정
    #             - 패키지 매니저 등
    
    # ---------------------------
    
    #   본문은 최대한 상세히 작성
    #   "무엇을", "왜"에 초점을 맞춰 설명
    #   본문에 여러줄 작성 시 "-"으로 구분
    
    # ---------------------------
    
    #   꼬리말 규칙
    #   "유형: #이슈 번호" 형태로 작성
    #   필수가 아닌 선택
    #
    # <Issue Tracker> 리스트
    #   Fixes     : 이슈 수정중(미해결 시)
    #   Resolves  : 이슈 해결
    #   Ref       : 참조할 이슈
    #   Related to: 해당 커밋 관련 이슈 번호(미해결 시)

---
+++ .eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint'],
+  settings: {
+    tailwindcss: { config: './tailwind.config.ts' },
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+
+    // * react
+    'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 1,
+    'import/no-extraneous-dependencies': 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external'],
+        'newlines-between': 'always',
+      },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+
+    // * typescript
+    '@typescript-eslint/no-shadow': ['warn'],
+
+    // * react-query
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'warn',
+    '@tanstack/query/stable-query-client': 'error',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-red-400 p-2 ">
+      test linting
+    </div>
+  );
+}
+
+export default App;
