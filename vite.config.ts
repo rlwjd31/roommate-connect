6c221dfdf6f2884907b8c6714e0feb9453eb036e
rlwjd31 <rlwjd31@naver.com>
2024-01-25 04:10:24 +0900

    🚀 [Feat]: absolute path 적용
    - vite에서 절대 경로를 생성하기 위해 vite-tsconfig-paths plugin 설치
    - src 하위 폴더를 읽어 자동으로 vite에 관한 path resolve alias 생성
    - vite 설정에 관한 path alias는 자동으로 생성되지만 tsconfig에는 수동으로 추가해야 함
    - @를 통해 정대 경로 자동완성이 안될 시 👇 설정 확인
    - vscode setting -> typescript > import module specifier를 non-relative로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { defineConfig, AliasOptions, Alias } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// ************* create vite path resolve alias *************
+// * src의 하위 directory를 읽어 sub directory를 가져와 아래 객체로 변환
+// * { find: '@libs', replacement: resolve(__dirname, 'src/libs') },
+const childDirectoriesOfSrc = fs
+  .readdirSync(path.resolve(__dirname, 'src'), {
+    withFileTypes: true,
+  })
+  .filter(childDir => childDir.isDirectory())
+  .map(
+    onlyDir =>
+      ({
+        find: `@${onlyDir.name}`,
+        replacement: path.resolve(__dirname, `src/${onlyDir.name}`),
+      }) as Alias,
+  );
+
+const alias: AliasOptions = [
+  { find: '@', replacement: path.resolve(__dirname, 'src') },
+  ...childDirectoriesOfSrc,
+];
+// **********************************************************
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias,
+  },
 });
