655374f0ce39ca5b32307ed58a132b6255ae6e0d
rlwjd31 <rlwjd31@naver.com>
2024-01-25 07:58:46 +0900

    🚀 [Feat]: MSW fetching 테스트 적용
    - db.json의 user로 msw 테스트 진행
    - 추후 App.tsx에 있는 예시 fetching 코드는 삭제 요함

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
+
 import './App.css';
 
 function App() {
+  const {
+    data: user,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['users', 'user'],
+    queryFn: async () => {
+      try {
+        const response = await fetch(
+          '/user/d0deb877-39dd-415e-93e3-2048ec1676c5',
+        );
+
+        if (!response.ok) throw new Error(`Couldn't find user`);
+
+        return await response.json();
+      } catch (err) {
+        console.error((err as Error).message);
+        throw new Error(`Couldn't find user`);
+      }
+    },
+  });
+
+  if (isLoading) return <h1>isLoading...</h1>;
+  if (error) return <h1>Error occured!</h1>;
+
   return (
     <div className="flex items-center justify-center bg-red-400 p-2 ">
       test linting
+      <img src={user.avatar} alt="user avatar" />
     </div>
   );
 }
---
+++ src/constants/apiEndpoints.ts
@@ -0,0 +1,9 @@
+type UserEndpointSType = {
+  user: string;
+};
+
+const userEndpoints: UserEndpointSType = {
+  user: '/user/:userId',
+};
+
+export { userEndpoints };
