2d7266fab15cefe451bd142469e6f333b6adbc01
Hanjun Kwon <kwondns@gmail.com>
2024-03-12 19:11:03 +0900

    🚀 [Feat]: 불필요 코드 제거
    - 임시로 작성했던 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,6 @@
-/* eslint-disable consistent-return */
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
-const isDev =
-  import.meta.env.MODE === 'development'
-
-// * run the mocking api before createRoot called
-async function enableMocking() {
-  if (import.meta.env.MODE !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('@/mocks/browser');
-
-  return worker.start();
-}
-
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        {isDev && <ReactQueryDevtools initialIsOpen={false} />}
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
