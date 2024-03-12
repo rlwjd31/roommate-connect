9913dcccf85ba50a9e7370643b55d84f52a27cb7
Hanjun Kwon <kwondns@gmail.com>
2024-03-12 19:10:38 +0900

    🚀 [Feat]: Sample Usage
    - Badge 사용 예

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
-
 import './App.css';
+import Badge from '@/components/atoms/Badge.tsx';
 
 function App() {
-  const {
-    data: user,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['users', 'user'],
-    queryFn: async () => {
-      try {
-        const response = await fetch(
-          '/user/d0deb877-39dd-415e-93e3-2048ec1676c5',
-        );
-
-        if (!response.ok) throw new Error(`Couldn't find user`);
-
-        return await response.json();
-      } catch (err) {
-        console.error((err as Error).message);
-        throw new Error(`Couldn't find user`);
-      }
-    },
-  });
-
-  if (isLoading) return <h1>isLoading...</h1>;
-  if (error) return <h1>Error occured!</h1>;
-
   return (
-    <div className="flex items-center justify-center bg-red-400 p-2 ">
+    <div className="bg-red-400 flex items-center justify-center p-2 ">
       test linting
-      <img src={user.avatar} alt="user avatar" />
+      <Badge.Button text="test" />
     </div>
   );
 }
