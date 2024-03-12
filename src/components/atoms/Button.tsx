ee6f83ed64895189d6c4702853efd047afe5b029
Hanjun Kwon <kwondns@gmail.com>
2024-03-12 18:37:10 +0900

    🚀 [Feat]: Atom Button Sample
    - Atom Button 샘플

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from 'react';
+
+type ButtonProps = ComponentProps<'button'>;
+export default function Button(props: ButtonProps) {
+  const { children, ...others } = props;
+  return (
+    <button type="button" {...others}>
+      {children}
+    </button>
+  );
+}
+
+Button.Fill = function ButtonFill(props: ButtonProps) {
+  const { children, ...others } = props;
+  return (
+    <button className="bg-brown" type="button" {...others}>
+      {children}
+    </button>
+  );
+};
