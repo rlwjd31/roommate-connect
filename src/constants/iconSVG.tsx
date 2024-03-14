7c35476621b1381c5b4043079df2ee1c3ffb2d9a
Hanjun Kwon <kwondns@gmail.com>
2024-03-14 15:36:29 +0900

    🚀 [Feat]: iconSVG
    - Icon 의 SVG 를 모아둔 모듈
    - 임시로 1가지의 SVG 만 추가해두어 추후 asset 추가 후 변경

--- src/constants/iconSVG.tsx
+++ src/constants/iconSVG.tsx
@@ -1,4 +1,8 @@
-export default {
+import { ReactNode } from 'react';
+
+import { IconType } from '@/types/icon.type.ts';
+
+const IconSVG: { [key in IconType]: ReactNode } = {
   character: <div />,
   logo: <div />,
   welcome: <div />,
@@ -15,4 +19,26 @@ export default {
   'single-family-house': <div />,
   smoke: <div />,
   'non-smoke': <div />,
+  'left-arrow': <div />,
+  'right-arrow': (
+    <svg
+      width="18"
+      height="16"
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 8L17 8M17 8L10 1M17 8L10 15"
+        stroke="#643927"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  close: <div />,
+  'expand-arrow': <div />,
 };
+
+export default IconSVG;
