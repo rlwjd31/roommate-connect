e8c9cadadf5104544d3b46a1e10d36824f359186
Hanjun Kwon <kwondns@gmail.com>
2024-03-14 15:08:13 +0900

    🚀 [Feat]: Type 정의
    - ColorType
    - IconType

---
+++ src/types/color.type.ts
@@ -0,0 +1,13 @@
+type ColorType =
+  | 'bg'
+  | 'subColor1'
+  | 'subColor2'
+  | 'brown'
+  | 'brown1'
+  | 'brown2'
+  | 'brown3'
+  | 'brown4'
+  | 'brown5'
+  | 'point';
+
+export default ColorType;
--- src/types/icon.type.ts
+++ src/types/icon.type.ts
@@ -1,3 +1,5 @@
+import ColorType from '@/types/color.type.ts';
+
 export type IconType =
   | 'character'
   | 'logo'
@@ -14,4 +16,13 @@ export type IconType =
   | 'apartment'
   | 'single-family-house'
   | 'smoke'
-  | 'non-smoke';
+  | 'non-smoke'
+  | 'right-arrow'
+  | 'left-arrow'
+  | 'expand-arrow'
+  | 'close';
+
+export type CustomIconType = {
+  fill?: ColorType;
+  stroke?: ColorType;
+};
