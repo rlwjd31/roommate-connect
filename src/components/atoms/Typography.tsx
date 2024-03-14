5cc45e4c5fdd95b9e488e0146667ca4fd63d22ec
Hanjun Kwon <kwondns@gmail.com>
2024-03-14 15:28:13 +0900

    🚀 Feat: Typography Atom 정의
    - 13가지의 Typography 생성
    - 반복문을 통해 컴포넌트 동적 생성

---
+++ src/components/atoms/Typography.tsx
@@ -0,0 +1,64 @@
+import React, { ComponentProps, ReactNode } from 'react';
+
+type TypographyProps =
+  | ComponentProps<'span'>
+  | ComponentProps<'p'>
+  | {
+      children: React.ReactNode;
+      className?: string;
+    };
+
+type TypoType =
+  | 'Head1'
+  | 'Head2'
+  | 'Head3'
+  | 'SubTitle1'
+  | 'SubTitle2'
+  | 'SubTitle3'
+  | 'P1'
+  | 'P2'
+  | 'P3'
+  | 'Span1'
+  | 'Span2'
+  | 'SpanMid1'
+  | 'SpanMid2';
+
+type TypographyComponentProps = {
+  [key in TypoType]: (props: TypographyProps) => ReactNode;
+};
+
+const typoType: {
+  type: TypoType;
+  defaultClassName: string;
+  component: string;
+}[] = [
+  { type: 'Head1', defaultClassName: 'text-Head1', component: 'h1' },
+  { type: 'Head2', defaultClassName: 'text-Head2', component: 'h2' },
+  { type: 'Head3', defaultClassName: 'text-Head3', component: 'h3' },
+
+  { type: 'SubTitle1', defaultClassName: 'text-SubTitle1', component: 'h4' },
+  { type: 'SubTitle2', defaultClassName: 'text-SubTitle2', component: 'h5' },
+  { type: 'SubTitle3', defaultClassName: 'text-SubTitle3', component: 'h6' },
+
+  { type: 'P1', defaultClassName: 'text-P1', component: 'p' },
+  { type: 'P2', defaultClassName: 'text-P2', component: 'p' },
+  { type: 'P3', defaultClassName: 'text-P3', component: 'p' },
+
+  { type: 'Span1', defaultClassName: 'text-Span1', component: 'span' },
+  { type: 'Span2', defaultClassName: 'text-Span2', component: 'span' },
+
+  { type: 'SpanMid1', defaultClassName: 'text-SpanMid1', component: 'span' },
+  { type: 'SpanMid2', defaultClassName: 'text-SpanMid2', component: 'span' },
+];
+
+const Typography = {} as TypographyComponentProps;
+typoType.forEach(({ type, component, defaultClassName }) => {
+  Typography[type] = ({ children, className, ...others }: TypographyProps) =>
+    React.createElement(
+      component,
+      { className: `${defaultClassName} ${className || ''} `, ...others },
+      children,
+    );
+});
+
+export default Typography;
