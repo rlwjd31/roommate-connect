0901c67ddcedb6163be7f8470a021f96f736e6ed
Hanjun Kwon <kwondns@gmail.com>
2024-03-14 15:37:44 +0900

    🚀 [Feat]: Icon Atom
    - SVGContainer 와 iconSVG 를 결합하여 자동으로 색상 변경 가능한 Icon 생성

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,11 +1,19 @@
-import { IconType } from '@/types/icon.type';
+import { CustomIconType, IconType } from '@/types/icon.type';
 import IconSVG from '@/constants/iconSVG';
+import SVGContainer from '@/components/atoms/SVGContainer.tsx';
 
-type IconProps = {
+export type IconProps = CustomIconType & {
   type: IconType;
+  className?: string;
 };
 
 export default function Icon(props: IconProps) {
-  const { type } = props;
-  return IconSVG[type];
+  const { type, fill = undefined, stroke = undefined, className = '' } = props;
+  return (
+    <SVGContainer className={className} fill={fill} stroke={stroke}>
+      {IconSVG[type]}
+    </SVGContainer>
+  );
 }
+
+Icon.defaultProps = { className: '' };
