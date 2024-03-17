4adbbd4a40d7023b4447aa499b95b40b5640d503
UHa <https00200@gmail.com>
2024-03-17 15:36:33 +0900

    🚀 [Feat]: Set Input type
    - input의 default type을 지정.

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -3,9 +3,10 @@ import { ComponentProps } from 'react';
 export type InputProps = ComponentProps<'input'>;
 
 export default function Input(props: InputProps) {
-  const { className, ...other } = props;
+  const { className, type = 'text', ...other } = props;
   return (
     <input
+      type={type}
       className={`block h-14 w-full rounded-lg border-[1px] border-solid border-brown p-[16px] focus:outline-none focus:ring-1 focus:ring-brown2 ${className || ''}`}
       {...other}
     />
