ed2dce9d3f54c898e7772cae1cd010c49c581cec
UHa <https00200@gmail.com>
2024-03-17 14:43:10 +0900

    🚀 [Feat]: Modify Input atom component
    - ComponentProps 사용해 Props type 다시 작성
    - input 의 기본 style 작성

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,21 +1,13 @@
-export type InputProps = {
-  id?: string;
-  type: 'text' | 'number' | 'email' | 'password';
-  placeholder: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-};
+import { ComponentProps } from 'react';
+
+export type InputProps = ComponentProps<'input'>;
 
 export default function Input(props: InputProps) {
-  const { id, type, placeholder, value, onChange } = props;
+  const { className, ...other } = props;
   return (
     <input
-      id={id}
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className=""
+      className={`block h-14 w-full rounded-lg border-[1px] border-solid border-brown p-[16px] focus:outline-none focus:ring-1 focus:ring-brown2 ${className || ''}`}
+      {...other}
     />
   );
 }
