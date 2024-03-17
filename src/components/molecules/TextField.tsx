73a75e8c02ac2ef5fa372b6293553c428b5bce99
UHa <https00200@gmail.com>
2024-03-17 17:36:27 +0900

    🚀 [Feat]: Add TextField molecules component
    - label과 input에 필요한 props를 기반으로 TextFieldProps 정의
    - options : 유효성 검사에 따른 errorMessage를 받아올 prop
    - helperText 유무에 따라 'invisible'이 추가되도록 수정

---
+++ src/components/molecules/TextField.tsx
@@ -0,0 +1,41 @@
+import { UseFormRegister, RegisterOptions, FieldValues } from 'react-hook-form';
+
+import Container from '../atoms/Container';
+import Input from '../atoms/Input';
+import Label from '../atoms/Label';
+import Typography from '../atoms/Typography';
+
+export type TextFieldProps = {
+  text: string;
+  name: string;
+  containerStyle?: string;
+  inputStyle?: string;
+  register: UseFormRegister<FieldValues>;
+  options: RegisterOptions;
+  helperText: string;
+};
+
+export default function TextField(props: TextFieldProps) {
+  const {
+    text,
+    name,
+    containerStyle,
+    inputStyle,
+    register,
+    options,
+    helperText,
+  } = props;
+  return (
+    <Container className={containerStyle}>
+      <Label>{text}</Label>
+      <Input className={inputStyle} {...register(name, options)} />
+      <Typography.Span2
+        className={`${helperText && 'invisible'} mt-[8px] block text-point`}
+      >
+        {helperText || '&nbsp'}
+      </Typography.Span2>
+    </Container>
+  );
+}
+
+TextField.defaultProps = { containerStyle: '', inputStyle: '' };
