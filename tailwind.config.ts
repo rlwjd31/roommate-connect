f6e9e205960eb5efd2838620c9864939350b4201
rlwjd31 <rlwjd31@naver.com>
2024-03-24 04:09:02 +0900

    🔨[Chore]: available base tailwind utility classes
    - 기존에 정의한 custom style을 확장하여 tailwind가 기본적으로
    제공하는 유틸리티 클래스를 사용할 수 있도록 바꿈
    - InputRange atom component의 track의 bg-transparent가
    안 되는 이슈 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,41 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      bg: '#FFFAF5',
-      subColor1: '#FFDBA5',
-      subColor2: '#FFB186',
-      brown: '#643927',
-      brown1: '#966E5D',
-      brown2: '#BCA79E',
-      brown3: '#DDCFC4',
-      brown4: '#FFF7F1',
-      brown5: '#FFFEFB',
-      point: '#FF7759',
+    extend: {
+      colors: {
+        bg: '#FFFAF5',
+        subColor1: '#FFDBA5',
+        subColor2: '#FFB186',
+        brown: '#643927',
+        brown1: '#966E5D',
+        brown2: '#BCA79E',
+        brown3: '#DDCFC4',
+        brown4: '#FFF7F1',
+        brown5: '#FFFEFB',
+        point: '#FF7759',
+      },
+      fontSize: {
+        Head1: ['3rem', { fontWeight: 700 }],
+        Head2: ['2rem', { fontWeight: 600 }],
+        Head3: ['1.5rem', { fontWeight: 600 }],
+        SubTitle1: ['1.25rem', { fontWeight: 600 }],
+        SubTitle2: ['1.125rem', { fontWeight: 600 }],
+        SubTitle3: ['1rem', { fontWeight: 600 }],
+        P1: ['1.25rem', { fontWeight: 400 }],
+        P2: ['1.125rem', { fontWeight: 400 }],
+        P3: ['1rem', { fontWeight: 400 }],
+        Span1: ['0.875rem', { fontWeight: 400 }],
+        Span2: ['0.75rem', { fontWeight: 400 }],
+        SpanMid1: ['0.875rem', { fontWeight: 500 }],
+        SpanMid2: ['0.75rem', { fontWeight: 500 }],
+      },
+      fontFamily: {
+        'Noto-Sans-KR': ['Noto Sans KR'],
+      },
     },
-    fontSize: {
-      Head1: ['3rem', { fontWeight: 700 }],
-      Head2: ['2rem', { fontWeight: 600 }],
-      Head3: ['1.5rem', { fontWeight: 600 }],
-      SubTitle1: ['1.25rem', { fontWeight: 600 }],
-      SubTitle2: ['1.125rem', { fontWeight: 600 }],
-      SubTitle3: ['1rem', { fontWeight: 600 }],
-      P1: ['1.25rem', { fontWeight: 400 }],
-      P2: ['1.125rem', { fontWeight: 400 }],
-      P3: ['1rem', { fontWeight: 400 }],
-      Span1: ['0.875rem', { fontWeight: 400 }],
-      Span2: ['0.75rem', { fontWeight: 400 }],
-      SpanMid1: ['0.875rem', { fontWeight: 500 }],
-      SpanMid2: ['0.75rem', { fontWeight: 500 }],
-    },
-    fontFamily: {
-      'Noto-Sans-KR': ['Noto Sans KR'],
-    },
-    extend: {},
   },
   plugins: [],
   safelist: [
