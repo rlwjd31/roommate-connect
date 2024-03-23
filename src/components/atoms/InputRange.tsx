189db4a79f93f5f965f4090695bf975bfa1f6829
rlwjd31 <rlwjd31@naver.com>
2024-03-24 04:16:54 +0900

    🚀 [Feat]: Add InputRange atom component
    - 기본 browser가 제공하는 thumb & track의 style을 죽임

---
+++ src/components/atoms/InputRange.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from 'react';
+
+import cls from '@/libs/cls';
+
+type InputRangeProps = ComponentProps<'input'>;
+
+// ! translate을 주니 thumb가 track의 위에서 보이게 됨
+const inputThumbStyle =
+  'slider-thumb:appearance-none slider-thumb:size-6 slider-thumb:rounded-full slider-thumb:bg-bg slider-thumb:ring-1 slider-thumb:ring-brown  slider-thumb:hover:bg-brown4 slider-thumb:hover:ring-2 slider-thumb:hover:ring-offset-2 slider-thumb:hover:ring-brown1 slider-thumb:cursor-pointer slider-thumb:translate-y-0 slider-thumb:pointer-events-auto';
+const inputTrackStyle =
+  'slider-track:appearance-none slider-track:bg-transparent';
+
+export default function InputRange({
+  min,
+  max,
+  step,
+  onChange,
+  value,
+  className,
+}: InputRangeProps) {
+  return (
+    <input
+      className={`pointer-events-none w-full appearance-none ${cls(inputThumbStyle, inputTrackStyle, className)}`}
+      onChange={onChange}
+      type="range"
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+    />
+  );
+}
