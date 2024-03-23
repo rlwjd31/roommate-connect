52f441106f261a52ff31604d8dc614182a7d2c3e
rlwjd31 <rlwjd31@naver.com>
2024-03-24 04:12:26 +0900

    🔨[Chore]: register input pseudo class selector
    - slider-track 👉🏻 &::-webkit-slider-runnable-track
    - slider-thumb 👉🏻 &::-webkit-slider-thumb

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,7 +37,20 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant('slider-thumb', [
+        '&::-webkit-slider-thumb', // chrome, safari, edge
+        '&::-moz-range-thumb', // firefox
+        '&::-ms-thumb', // IE
+      ]);
+      addVariant('slider-track', [
+        '&::-webkit-slider-runnable-track', // chrome, safari, edge
+        '&::-moz-range-track', // firefox
+        '&::-ms-track', // IE
+      ]);
+    }),
+  ],
   safelist: [
     {
       pattern:
