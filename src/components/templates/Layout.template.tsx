bbfe90fba5787869e432c3621e75d1ad8f67feeb
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-26 22:54:59 +0900

    🔨 [Chore]: Patch Apply
    - HC-44-layout-생성 Patch

---
+++ src/components/templates/Layout.template.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+import cn from '@/libs/cn.ts';
+
+export default function LayoutTemplate() {
+  return (
+    <main
+      className={cn(
+        'flex flex-col relative max-w-[1200px] mx-auto h-screen px-8 pt-[147px]',
+      )}
+    >
+      <Outlet />
+    </main>
+  );
+}
---
+++ src/components/templates/SignProfileLayout.template.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Container from '@/components/atoms/Container.tsx';
+import IconButton from '@/components/molecules/IconButton.tsx';
+import Typography from '@/components/atoms/Typography.tsx';
+
+type SignProfileLayoutTemplateProps = {
+  children: React.ReactNode;
+};
+export default function SignProfileLayoutTemplate(
+  props: Readonly<SignProfileLayoutTemplateProps>,
+) {
+  const { children } = props;
+  return (
+    <Container.FlexRow className="max-h-[816px] grow justify-between pt-[41px]">
+      {/* TODO Container To Stepper */}
+      <Container className="w-[188px]">
+        <span>Stepper </span>
+      </Container>
+      <Container.FlexCol className="justify-between">
+        <Container className="w-[894px]">{children}</Container>
+        <Container.FlexRow className="justify-end gap-x-3">
+          {/* TODO right-arrow to left-arrow */}
+          <IconButton.Outline
+            className="flex-row-reverse gap-x-[10px] rounded-[32px] px-[30px] py-[15px]"
+            iconType="right-arrow"
+          >
+            <Typography.P1>이전</Typography.P1>
+          </IconButton.Outline>
+          <IconButton.Fill
+            className="gap-x-[10px] rounded-[32px] px-[30px] py-[15px]"
+            iconType="right-arrow"
+            stroke="bg"
+          >
+            <Typography.P1 className="text-bg">다음</Typography.P1>
+          </IconButton.Fill>
+        </Container.FlexRow>
+      </Container.FlexCol>
+    </Container.FlexRow>
+  );
+}
