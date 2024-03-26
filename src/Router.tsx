7d9f9c7fa37b8f5c9160ca14cbd8b5c56da5ceda
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-26 19:55:17 +0900

    🚀 [Feat]: 임시 라우터 생성
    - signin, siginup, signup-profile, about, component-test
    - 5가지에 대한 임시 라우트 추가
    - SignLayout 중복으로 사용되는 레이아웃은 묶어 사용

---
+++ src/Router.tsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import LayoutTemplate from '@/components/templates/Layout.template';
+import ComponentTest from '@/components/pages/ComponentTest.tsx';
+import SignLayoutTemplate from '@/components/templates/SignLayout.template';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    index: true,
+    element: <span>서비스 소개 및 사용방법</span>,
+  },
+  {
+    element: <LayoutTemplate />,
+    children: [
+      {
+        // ! Sign 레이아웃 적용
+        path: 'sign',
+        element: <SignLayoutTemplate />,
+        children: [
+          {
+            path: 'in',
+            element: <span>로그인 / 소셜 로그인 /회원가입 이동</span>,
+          },
+          {
+            path: 'up',
+            element: (
+              <span>이메일 인증, 암호 설정, 이름, 주소, 전화번호 인증</span>
+            ),
+          },
+        ],
+      },
+      {
+        // ! 기본 레이아웃 적용
+        path: 'signup-profile',
+        element: <span>프로필 설정, 회원가입 완료</span>,
+      },
+      {
+        path: 'component-test',
+        element: <ComponentTest />,
+      },
+    ],
+  },
+]);
+export default function Router() {
+  return <RouterProvider router={router} />;
+}
