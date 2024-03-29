1264b98c854ba1a227775d03ae2008586df62ddd
kwondns <kwondns@gmail.com>
2024-03-29 21:57:29 +0900

    💄 [Design]: SignLayout 수정
    - 하나의 컨테이너를 추가하여 레이아웃의 Input 영역의 굴곡진 모습 적용

--- src/components/templates/SignLayout.template.tsx
+++ src/components/templates/SignLayout.template.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
 import cn from '@/libs/cn';
+import Container from '@/components/atoms/Container';
 
 export default function SignLayoutTemplate() {
   return (
     <>
-      <div
+      <Container
         className={cn(
-          'fixed left-0 top-0 z-[-9999] h-screen w-[50vw] bg-subColor1',
+          'fixed left-0 top-0 z-[-9999] h-screen w-[51vw] bg-subColor1',
+        )}
+      />
+      <Container
+        className={cn(
+          'fixed right-0 top-0 z-[-9998] h-screen w-[50vw] bg-bg rounded-xl',
         )}
       />
       <Outlet />
