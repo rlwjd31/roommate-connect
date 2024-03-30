6221aacc2212ca34840e0a117a6d6d47ca837ef1
rlwjd31 <rlwjd31@naver.com>
2024-03-30 19:46:16 +0900

    🎨 [Style]: rename dropdown -> selector
    ##### Subject 50 characters ################# -> |
    
    # Body Message
    ######## Body 72 characters ####################################### -> |
    
    # Issue Tracker Number or URL
    
    # --- COMMIT END ---
    #   제목 첫 글자는 대문자
    #   제목에 마침표, 특수기호 X
    #   간결하고 요점만 작성
    
    # <subject> 리스트
    #   🚀feat    : 새로 추가되는 기능
    #   🚑️fix     : 버그 수정 및 단순 텍스트 수정
    #   ♻️refactor: 리팩토링
    #   🎨style   : 코드 스타일 수정(포멧팅) 코드 내용 변경 X
    #   📝docs    : 문서 추가 및 수정
    #   💡comment : 주석 변경
    #   💄design  : CSS 스타일링 수정
    #   🎉init    : 프로젝트 초기 설정
    #   🚚file    : 파일 및 폴더 수정, 이동, 삭제
    #   🐛bug     : 버그 reporting할 때, @FIXME 주석 태그 삽입
    #   🔨chore   : 기타 변경사항
    #             - 빌드 스크립트 수정
    #             - asset 수정
    #             - 패키지 매니저 등
    
    # ---------------------------
    
    #   본문은 최대한 상세히 작성
    #   "무엇을", "왜"에 초점을 맞춰 설명
    #   본문에 여러줄 작성 시 "-"으로 구분
    
    # ---------------------------
    
    #   꼬리말 규칙
    #   "유형: #이슈 번호" 형태로 작성
    #   필수가 아닌 선택
    #
    # <Issue Tracker> 리스트
    #   Fixes     : 이슈 수정중(미해결 시)
    #   Resolves  : 이슈 해결
    #   Ref       : 참조할 이슈
    #   Related to: 해당 커밋 관련 이슈 번호(미해결 시)

--- src/components/atoms/SelectorItem.tsx
+++ src/components/atoms/SelectorItem.tsx
@@ -3,18 +3,18 @@ import { ComponentProps } from 'react';
 import cn from '@/libs/cn';
 import Button from '@/components/atoms/Button';
 
-type DropdownItemProps = ComponentProps<'li'>;
+type SelectorItemProps = ComponentProps<'li'>;
 
-const dropdownItemStyle =
+const selectorItemStyle =
   'last:border-b-0 flex w-full items-center justify-center border-b bg-bg border-brown text-brown hover:bg-brown3';
 
-export default function DropdownItem({
+export default function SelectorItem({
   children,
   className = '',
   ...others
-}: DropdownItemProps) {
+}: SelectorItemProps) {
   return (
-    <li className={cn(dropdownItemStyle, className)} {...others}>
+    <li className={cn(selectorItemStyle, className)} {...others}>
       <Button.Ghost className="w-full cursor-pointer justify-center bg-bg p-5">
         {children}
       </Button.Ghost>
