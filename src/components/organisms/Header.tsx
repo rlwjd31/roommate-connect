bacff46746e816b45d45a3701403391808355693
rlwjd31 <rlwjd31@naver.com>
2024-04-07 01:19:07 +0900

    🚑️ [Fix]: Header fixed 적용
    - fixed적용
    - navItem hover text color brown1로 적용

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, useState } from 'react';
+import { ComponentProps } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Container from '@/components/atoms/Container';
@@ -22,35 +22,34 @@ export default function Header({ className, isLogin, ...others }: Props) {
   const location = useLocation();
 
   return (
-    <header
-      className="mx-auto flex max-w-[1200px] items-center justify-between bg-bg px-8"
-      {...others}
-    >
-      <Link to="/">
-        <Icon type="logo" />
-      </Link>
-      {isLogin && (
-        <>
-          <Container.FlexRow className="gap-9">
-            {navItems.map(({ name, path }) => (
-              <Link key={name} to={path}>
-                <Typography.SpanMid1
-                  className={cn(
-                    'text-[0.9375rem] font-semibold uppercase tracking-widest',
-                    location.pathname !== path && 'text-brown2',
-                  )}
-                >
-                  {name}
-                </Typography.SpanMid1>
-              </Link>
-            ))}
-          </Container.FlexRow>
-          <Container.FlexRow className="items-center justify-between gap-7">
-            <IconButton button="Ghost" iconType="alarm-exist" />
-            <IconButton button="Ghost" iconType="avartar" />
-          </Container.FlexRow>
-        </>
-      )}
+    <header className="fixed left-0 top-0 z-50 w-screen bg-bg" {...others}>
+      <Container.FlexRow className="mx-auto w-full max-w-[1200px] items-center justify-between px-8">
+        <Link to="/">
+          <Icon type="logo" />
+        </Link>
+        {isLogin && (
+          <>
+            <Container.FlexRow className="gap-9">
+              {navItems.map(({ name, path }) => (
+                <Link key={name} to={path}>
+                  <Typography.SpanMid1
+                    className={cn(
+                      'text-[0.9375rem] font-semibold uppercase tracking-widest hover:text-brown1',
+                      location.pathname !== path && 'text-brown2',
+                    )}
+                  >
+                    {name}
+                  </Typography.SpanMid1>
+                </Link>
+              ))}
+            </Container.FlexRow>
+            <Container.FlexRow className="items-center justify-between gap-7">
+              <IconButton button="Ghost" iconType="alarm-exist" />
+              <IconButton button="Ghost" iconType="avartar" />
+            </Container.FlexRow>
+          </>
+        )}
+      </Container.FlexRow>
     </header>
   );
 }
