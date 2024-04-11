ee2625243d1313db1d7c6afd2e8590bb3d5ca60f
rlwjd31 <rlwjd31@naver.com>
2024-04-11 20:33:03 +0900

    🔨 [Chore]: apply patch issue-3
    - 052126bacf4d166392666892ee413d3c25e76083

--- src/components/molecules/TextField.tsx
+++ src/components/molecules/TextField.tsx
@@ -1,28 +1,44 @@
-import { RegisterOptions, useFormContext } from 'react-hook-form';
+import { FieldValues, RegisterOptions, useFormContext } from 'react-hook-form';
 
 import Container from '@/components/atoms/Container';
 import Input, { InputProps } from '@/components/atoms/Input';
 import Label from '@/components/atoms/Label';
 import Typography from '@/components/atoms/Typography';
 
-export type TextFieldProps = InputProps & {
-  text: string;
-  name: string;
+export type TextFieldProps<T extends FieldValues> = InputProps & {
+  name: keyof T;
+  labelName?: string;
+  options?: RegisterOptions;
+  activeWatch?: boolean;
   containerStyle?: string;
   inputStyle?: string;
-  options?: RegisterOptions;
 };
 
-export default function TextField(props: TextFieldProps) {
-  const { text, type, name, containerStyle, inputStyle, placeholder, options } =
-    props;
+export default function TextField<T extends FieldValues>(props: TextFieldProps<T>) {
+  const {
+    labelName,
+    type,
+    name,
+    containerStyle,
+    inputStyle,
+    placeholder,
+    options,
+    activeWatch,
+  } = props;
 
-  const { register, formState } = useFormContext();
+  const { register, formState, watch } = useFormContext();
+
+  if (activeWatch) console.log(watch(name));
 
   return (
     <Container className={containerStyle}>
-      <Label>{text}</Label>
-      <Input type={type} className={inputStyle} placeholder={placeholder} {...register(name, options)} />
+      <Label>{labelName}</Label>
+      <Input
+        type={type}
+        className={inputStyle}
+        placeholder={placeholder}
+        {...register(name, options)}
+      />
       <Typography.Span2
         className={`${!formState.errors[name]?.message && 'invisible'} mt-[8px] block text-point`}
       >
@@ -32,4 +48,10 @@ export default function TextField(props: TextFieldProps) {
   );
 }
 
-TextField.defaultProps = { containerStyle: '', inputStyle: '', options: {} };
+TextField.defaultProps = {
+  containerStyle: '',
+  inputStyle: '',
+  options: {},
+  labelName: '',
+  activeWatch: false,
+};
