fd98c16c054752f93875a7cdeeefccaa84758cd0
kwondns <kwondns@gmail.com>
2024-04-13 23:35:49 +0900

    🚑️ [Fix]: 훅 이름 변경
    - 명확한 이름으로 훅 변경

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -2,12 +2,16 @@ import { useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 
 import SignInTemplate from '@/components/templates/SignInTemplate';
-import { useAuth, userAdditionalInfo, useUpdateUser } from '@/hooks/useSignIn';
+import {
+  useSignInState,
+  userAdditionalInfo,
+  useUpdateUser,
+} from '@/hooks/useSignIn';
 
 export default function SignIn() {
   const { updateUser } = useUpdateUser();
   const queryClient = useQueryClient();
-  const session = useAuth();
+  const session = useSignInState();
 
   useEffect(() => {
     if (session && session.user.app_metadata.provider !== 'email') {
