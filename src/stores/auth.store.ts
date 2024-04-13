024588746bcbc4553069087238a6e87b1724f720
kwondns <kwondns@gmail.com>
2024-04-13 23:43:02 +0900

    🚀 [Feat]: User 상태 데이터를 로컬스토리지와 동기화
    - Supabase 에서 인증 시 자동으로 LocalStorage 에 데이터 저장
    - 새로 고침 후 LocalStorage 에 데이터는 남아있지만 Recoil 상태는 초기화
    - 따라서 UserAtom 에 AtomEffect 를 추가하여 LocalStorage 의 데이터와 동기화
    - AtomEffect 는 useEffect 와 마찬가지로 초기화 시, 상태의 변화 시 동작을 한다.
    - Atom 이 리셋이 되면 default 의 값으로 지정되고 후에 초기화가 수행된다.

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,8 +1,31 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
 
-import { UserType } from '@/types/auth.type';
+import { UserLocalStorageType, UserType } from '@/types/auth.type';
+
+const UserAtomEffect =
+  (key: string): AtomEffect<UserType | null> =>
+  ({ setSelf }) => {
+    const storageUser = localStorage.getItem(key);
+    if (storageUser !== null) {
+      const userObject = JSON.parse(storageUser) as UserLocalStorageType;
+      const { id } = userObject.user;
+      const { name, nickname, gender, email, avatar, birth, status } =
+        userObject.user.user_metadata;
+      setSelf({
+        id,
+        name,
+        nickname,
+        gender,
+        email,
+        avatar,
+        birth,
+        status,
+      });
+    }
+  };
 
 export const UserAtom = atom<UserType | null>({
   key: 'userAtom',
   default: null,
+  effects: [UserAtomEffect('sb-vkgzfgadnhdgapepgjlp-auth-token')],
 });
