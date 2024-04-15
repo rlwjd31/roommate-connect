ca651964182cb8527734702605639d6858373674
rlwjd31 <rlwjd31@naver.com>
2024-04-16 00:53:09 +0900

    🚑️ [Fix]: Molecule StepNav take className prop

--- src/components/molecules/StepNavigation.tsx
+++ src/components/molecules/StepNavigation.tsx
@@ -1,16 +1,21 @@
+import { ComponentProps } from 'react';
+
 import Container from '@/components/atoms/Container';
 import StepLink from '@/components/atoms/StepLink';
+import cn from '@/libs/cn';
 
-type Props = {
+type Props = ComponentProps<'div'> & {
   contents: {
     labelName: string;
     isActive: boolean;
   }[];
 };
 
-export default function StepNavigation({ contents }: Props) {
+export default function StepNavigation({ contents, className }: Props) {
   return (
-    <Container.FlexCol className="relative w-fit justify-between">
+    <Container.FlexCol
+      className={cn('relative w-fit justify-between', className)}
+    >
       {contents.map(({ labelName, isActive }) => (
         <StepLink key={labelName} labelName={labelName} isActive={isActive} />
       ))}
