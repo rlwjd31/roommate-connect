f40eea39ea9f71343126b0668fe8644670eac782
rlwjd31 <rlwjd31@naver.com>
2024-04-16 02:31:46 +0900

    🚀 [Feat]: Add Molecule BadgeButtons
    - UI에 badge를 뿌려주는 곳이 많아 코드 중복제거를 위해 molecule로 작성
    - div onClick과 겹쳐 div에 관한 onClick은 타입에서 뺌
    - 해당 컴포넌트는 단순히 badgeButton을 모아 rendering하므로 molecule로 구성

---
+++ src/types/typo.type.ts
@@ -0,0 +1,16 @@
+type TypoType =
+  | 'Head1'
+  | 'Head2'
+  | 'Head3'
+  | 'SubTitle1'
+  | 'SubTitle2'
+  | 'SubTitle3'
+  | 'P1'
+  | 'P2'
+  | 'P3'
+  | 'Span1'
+  | 'Span2'
+  | 'SpanMid1'
+  | 'SpanMid2';
+
+export default TypoType;
