2847419d2ec0079f79d9c511f8fb116e83c7a10e
UHa <https00200@gmail.com>
2024-04-16 21:46:43 +0900

    🎨 [Style] : Detach the default style of the input component
    - Input atom component에서 정하는 input의 기본스타일을 따로 변수로 빼서 추가함.

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -4,10 +4,12 @@ export type InputProps = ComponentProps<'input'>;
 const Input = forwardRef<HTMLInputElement, HTMLProps<HTMLInputElement>>(
   (props, ref) => {
     const { className, type = 'text', ...other } = props;
+    const inputBaseStyle =
+      'block h-14 w-full rounded-lg border-[1px] border-solid border-brown p-[16px] focus:outline-none focus:ring-1 focus:ring-brown2 bg-transparent';
     return (
       <input
         type={type}
-        className={`block h-14 w-full rounded-lg border-[1px] border-solid border-brown p-[16px] focus:outline-none focus:ring-1 focus:ring-brown2 ${className || ''}`}
+        className={`${inputBaseStyle} ${className || ''}`}
         ref={ref}
         {...other}
       />
