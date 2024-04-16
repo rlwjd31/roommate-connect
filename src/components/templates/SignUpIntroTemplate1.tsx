fc30a0cc00fe95a398c5cf903753c26e913f6156
UHa <https00200@gmail.com>
2024-04-16 21:48:12 +0900

    🚀 [Feat] : Modify SignUpIntroTemplate UI
    - 디자인 수정에 따른 회원가입 페이지 UI수정
    - 각 요청필드에 유효성 검사 추가

--- src/components/templates/SignUpIntroTemplate1.tsx
+++ src/components/templates/SignUpIntroTemplate1.tsx
@@ -24,31 +24,40 @@ export default function SignUpIntroTemplate1({
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <Container.FlexCol className="gap-[1.625rem]">
             <TextField
-              text="이메일"
-              type="email"
-              name="email"
-              options={{ required: {value : true, message: '써라.'} }}
-              placeholder="이메일 입력"
-              inputStyle="bg-transparent mt-[1rem]"
+              labelName="이름"
+              type="text"
+              name="name"
+              options={{
+                required: '필수 입력 사항입니다.',
+                minLength: {
+                  value: 2,
+                  message: '최소 2글자 이상 입력해주세요.',
+                },
+              }}
+              placeholder="이름 입력"
+              inputStyle="mt-[1rem]"
             />
             <TextField
-              text="비밀번호"
-              type="password"
-              name="password"
-              options={{ required: true }}
-              placeholder="비밀번호 입력"
-              inputStyle="bg-transparent mt-[1rem]"
-            />
-            <TextField
-              text="비밀번호 재입력"
-              type="password"
-              name="verifyPassword"
-              options={{ required: true }}
-              placeholder="비밀번호 재입력"
-              inputStyle="bg-transparent mt-[1rem]"
+              labelName="주민등록번호"
+              type="text"
+              name="identificationNumber"
+              options={{
+                required: '필수 입력 사항입니다.',
+                minLength: { value: 7, message: '7자리 모두 입력해주세요.' },
+                maxLength: { value: 7, message: '7자리만 입력해주세요.' },
+                pattern: {
+                  value: /^[0-9]{6}[-\s.]?[1-4]{1}$/,
+                  message: '숫자만 입력 가능합니다.',
+                },
+              }}
+              placeholder="7자리 ex) 990101-1******"
+              inputStyle="mt-[1rem]"
             />
           </Container.FlexCol>
-          <Button.Fill type="submit" className=" w-full rounded-[10px]">
+          <Button.Fill
+            type="submit"
+            className=" mt-[3.25rem] w-full rounded-[10px]"
+          >
             <Typography.P3 className="mx-auto my-[1rem] text-[#F4E7DB]">
               다음
             </Typography.P3>
