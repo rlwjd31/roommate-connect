77b7a7007a85894b2eb4bdcb475c3349df1d2b06
rlwjd31 <rlwjd31@naver.com>
2024-04-30 18:26:21 +0900

    🚀 [Feat]: modalType에 따른 modal state접근 및 업데이트 기능

--- src/stores/globalModal.store.ts
+++ src/stores/globalModal.store.ts
@@ -9,7 +9,6 @@ import {
   ProfileModalState,
 } from '@/types/modal.type';
 
-// ! TODO: 나중에 isOpen을 false로 바꾸어야 한다.
 export const GlobalModalAtom = atom<GlobalModalState>({
   key: 'globalModalstate',
   default: {
@@ -47,6 +46,46 @@ export const ProfileModalAtom = atom<ProfileModalState>({
   default: {
     type: 'Profile',
   },
+});
+export const ModalSelector = selectorFamily<
+  ModalStateByType[keyof ModalStateByType],
+  ModalType
+>({
+  key: 'modalPropsByType',
+  get:
+    <P extends ModalType>(modalType: P) =>
+    ({ get }) => {
+      switch (modalType) {
+        case 'Alert':
+          return get(AlertModalAtom) as AlertModalState;
+        case 'Confirm':
+          return get(ConfirmModalAtom) as ConfirmModalState;
+        case 'Profile':
+          return get(ProfileModalAtom) as ProfileModalState;
+        default:
+          errorSelector('Undefined cannot be a value of ModalType.');
+          return get(AlertModalAtom) as AlertModalState;
+      }
+    },
+  set:
+    modalType =>
+    ({ set }, newModalState) => {
+      switch (modalType) {
+        case 'Alert':
+          set(AlertModalAtom, newModalState as AlertModalState);
+          break;
+        case 'Confirm':
+          set(ConfirmModalAtom, newModalState as ConfirmModalState);
+          break;
+        case 'Profile':
+          set(ProfileModalAtom, newModalState as ProfileModalState);
+          break;
+        default:
+          // eslint-disable-next-line no-console
+          console.warn(`Received unexpected modal type: ${modalType}`);
+          errorSelector(`Received unexpected modal type: ${modalType}`);
+      }
+    },
 });
 
 export default {};
