fa0a020fd65b9d206651cfd3018d1af2b5c0d32a
kimcode <sophy0219@naver.com>
2024-05-14 23:40:39 +0900

    ♻️ [Refactor]: roommate appeal 상태 rendering 간결화

--- src/components/organisms/modals/RoommateApplyModal.tsx
+++ src/components/organisms/modals/RoommateApplyModal.tsx
@@ -5,6 +5,14 @@ import Typography from '@/components/atoms/Typography';
 import Container from '@/components/atoms/Container';
 
 export default function RoommateApplyModal() {
+  const roommateAppeals = [
+    '1명',
+    '남성',
+    '잠귀 어두운 분',
+    '청소 자주해요',
+    '늦게 자요',
+  ];
+
   return (
     <ModalBackdrop modalType="RoommateApply">
       <Container.FlexCol className="max-h-[808px] max-w-[678px] bg-bg p-8 text-brown">
@@ -15,21 +23,14 @@ export default function RoommateApplyModal() {
           <Container.FlexCol className="gap-6">
             <Typography.SubTitle2>내가 원하는 룸메이트</Typography.SubTitle2>
             <Container.FlexRow className="h-[8.75rem] w-full flex-wrap items-start gap-2 rounded-lg bg-brown3 p-6">
-              <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
-                1명
-              </Badge.Outline>
-              <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
-                남성
-              </Badge.Outline>
-              <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
-                잠귀 어두운 분
-              </Badge.Outline>
-              <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
-                청소 자주해요
-              </Badge.Outline>
-              <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
-                늦게 자요
-              </Badge.Outline>
+              {roommateAppeals.map(appeal => (
+                <Badge.Outline
+                  key={appeal}
+                  className="rounded-3xl px-[20px] py-[10px]"
+                >
+                  {appeal}
+                </Badge.Outline>
+              ))}
             </Container.FlexRow>
           </Container.FlexCol>
           <Container.FlexCol className="gap-6">
@@ -37,8 +38,6 @@ export default function RoommateApplyModal() {
             <Container.FlexCol>
               <textarea
                 placeholder="나를 소개하는 글을 작성해보세요."
-                name=""
-                id=""
                 className="h-72 w-full rounded-[15px] bg-brown3 p-6 placeholder:text-brown2"
               />
             </Container.FlexCol>
