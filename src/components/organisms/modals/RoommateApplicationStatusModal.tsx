9d3b2f1f36aef845a441c9978d82b4bd0a906ef8
kimcode <sophy0219@naver.com>
2024-05-17 19:33:11 +0900

    🚀 [Feat]: RoommateApplicationStatus(룸메이트 신청 현황 모달) UI
    ##### Subject 50 characters ################# -> |
    # Body Message
    ######## Body 72 characters ####################################### -> |
    # Issue Tracker Number or URL
    # --- COMMIT END ---
    #   제목 첫 글자는 대문자
    #   제목에 마침표, 특수기호 X
    #   간결하고 요점만 작성
    # <subject> 리스트
    #   🚀feat    : 새로 추가되는 기능
    #   🚑️fix     : 버그 수정 및 단순 텍스트 수정
    #   ♻️refactor: 리팩토링
    #   🎨style   : 코드 스타일 수정(포멧팅) 코드 내용 변경 X
    #   📝docs    : 문서 추가 및 수정
    #   💡comment : 주석 변경
    #   💄design  : CSS 스타일링 수정
    #   🎉init    : 프로젝트 초기 설정
    #   🚚file    : 파일 및 폴더 수정, 이동, 삭제
    #   🐛bug     : 버그 reporting할 때, @FIXME 주석 태그 삽입
    #   🔨chore   : 기타 변경사항
    #             - 빌드 스크립트 수정
    #             - asset 수정
    #             - 패키지 매니저 등
    # ---------------------------
    #   본문은 최대한 상세히 작성
    #   "무엇을", "왜"에 초점을 맞춰 설명
    #   본문에 여러줄 작성 시 "-"으로 구분
    # ---------------------------
    #   꼬리말 규칙
    #   "유형: #이슈 번호" 형태로 작성
    #   필수가 아닌 선택
    #
    # <Issue Tracker> 리스트
    #   Fixes     : 이슈 수정중(미해결 시)
    #   Resolves  : 이슈 해결
    #   Ref       : 참조할 이슈
    #   Related to: 해당 커밋 관련 이슈 번호(미해결 시)

--- src/components/organisms/modals/RoommateApplicationStatusModal.tsx
+++ src/components/organisms/modals/RoommateApplicationStatusModal.tsx
@@ -3,23 +3,63 @@ import Container from '@/components/atoms/Container';
 import Typography from '@/components/atoms/Typography';
 import IconButton from '@/components/molecules/IconButton';
 import Button from '@/components/atoms/Button';
+import Badge from '@/components/atoms/Badge';
+import Icon from '@/components/atoms/Icon';
 
 export default function RoomMateApplicationStatus() {
   return (
     <ModalBackdrop modalType="RoomMateApplication">
       <Container.FlexCol className="max-h-[566px] w-full max-w-[874px] bg-bg p-8 text-brown">
-        <Container.FlexRow className="justify-between">
-          <Typography.Head3>신청 현황</Typography.Head3>
-          <IconButton iconType="close" button="Outline" />
+        <Container.FlexRow className="justify-between border-b-[0.5px] border-brown pb-7">
+          <Typography.Head3 className="">신청 현황</Typography.Head3>
+          <IconButton className="size-6" iconType="close" button="Ghost" />
         </Container.FlexRow>
         <Container.FlexCol className="pt-6">
-          <Typography.SubTitle2>신청한 인원 1명</Typography.SubTitle2>
-          <Container.FlexCol className="gap-6">
-            <div className="">user data</div>
-            <Container.FlexRow>
-              <Button.Outline>1:1 대화</Button.Outline>
-              <Button.Outline>거절</Button.Outline>
-              <button type="button">수락</button>
+          <Typography.SubTitle2 className="mb-6">
+            신청한 인원 1명
+          </Typography.SubTitle2>
+          <Container.FlexCol className="max-h-96 w-full items-start gap-8 rounded-lg bg-brown6 p-6">
+            <Container.FlexRow className="max-w-xl gap-7">
+              {/* <Container.FlexRow className="size-[70px] rounded-full bg-brown" /> */}
+              <Icon className="[&>svg]:size-16 " type="avartar" />
+              <Container.FlexCol className="gap-7">
+                <Typography.Head3>user123</Typography.Head3>
+                <Container.FlexRow className="flex flex-wrap gap-2.5">
+                  <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
+                    1명
+                  </Badge.Outline>
+                  <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
+                    남성
+                  </Badge.Outline>
+                  <div className="w-full" />
+                  <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
+                    잠귀 어두운 분
+                  </Badge.Outline>
+                  <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
+                    청소 자주해요
+                  </Badge.Outline>
+                  <Badge.Outline className="rounded-3xl px-[20px] py-[10px]">
+                    늦게 자요
+                  </Badge.Outline>
+                </Container.FlexRow>
+                <Typography.P3 className="text-base">
+                  안녕하세요! 1년 6개월 동안 사는 것을 희망하고 조용히 지낼 수
+                  있습니다. <br /> 집이 좋아보여서 신청해봅니다!
+                </Typography.P3>
+              </Container.FlexCol>
+            </Container.FlexRow>
+            <Container.FlexRow className="w-full flex-wrap items-start justify-between gap-2 border-t-[0.5px] border-brown pt-7">
+              <Button.Outline className="rounded-3xl bg-brown6 px-11 py-3">
+                1:1 대화
+              </Button.Outline>
+              <Container.FlexRow className="gap-3">
+                <Button.Outline className="rounded-3xl bg-brown6 px-11 py-3">
+                  거절
+                </Button.Outline>
+                <Button.Fill className="rounded-3xl px-11 py-3 text-brown3">
+                  수락
+                </Button.Fill>
+              </Container.FlexRow>
             </Container.FlexRow>
           </Container.FlexCol>
         </Container.FlexCol>
