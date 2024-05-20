4808bf7f6b97c366ee9118817642005b2ef5e708
kimcode <sophy0219@naver.com>
2024-05-20 16:10:40 +0900

    🚀 [Feat] : 전역모달에 룸메이트 신청 현황 모달 추가

--- src/components/templates/GlobalModal.tsx
+++ src/components/templates/GlobalModal.tsx
@@ -3,16 +3,18 @@ import { useRecoilValue } from 'recoil';
 import AlertModal from '@/components/organisms/modals/AlertModal';
 import ConfirmModal from '@/components/organisms/modals/ConfirmModal';
 import ProfileModal from '@/components/organisms/modals/ProfileModal';
+import RoomMateApplicationStatus from '../organisms/modals/RoommateApplicationStatusModal';
 import { GlobalModalAtom } from '@/stores/globalModal.store';
 
 export default function GlobalModal() {
-  const  modalType  = useRecoilValue(GlobalModalAtom);
+  const modalType = useRecoilValue(GlobalModalAtom);
 
   // eslint-disable-next-line react-refresh/only-export-components
   const TypesOfModals = {
     Alert: AlertModal,
     Confirm: ConfirmModal,
     Profile: ProfileModal,
+    RoommateApplicationStatus: RoomMateApplicationStatus,
   };
 
   const SelectedModal = TypesOfModals[modalType];
