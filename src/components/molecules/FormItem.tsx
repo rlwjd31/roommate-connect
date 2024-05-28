a181ce7f5f74561f52f641d64e04deeab7004cab
kimcode <sophy0219@naver.com>
2024-05-29 02:03:12 +0900

    🚀 [Feat]: Molecules FormItemTextAreaField 추가

--- src/components/molecules/FormItem.tsx
+++ src/components/molecules/FormItem.tsx
@@ -2,6 +2,9 @@ import { Controller, FieldValues, useFormContext } from 'react-hook-form';
 
 import TextField, { TextFieldProps } from '@/components/molecules/TextField';
 import Input from '@/components/atoms/Input';
+import TextAreaField, {
+  TextAreaFieldProps,
+} from '@/components/molecules/TextAreaField';
 
 export default function FormItem() {}
 
@@ -40,4 +43,46 @@ FormItem.Hidden = function FormItemPassword<T extends FieldValues>(
     );
 
   return <span>Name 속성이 필요합니다</span>;
+};
+
+// // ! controller로 감쌀 필요가 없이 register
+// FormItem.TextArea = function FormItemTextArea<T extends FieldValues>(
+//   props: TextAreaFieldProps<T>,
+// ) {};
+
+// FormItem.TextAreaWithState => controller로 감싸고(control을 이용하는 것 )
+FormItem.TextAreaField = function FormItemTextAreaField<T extends FieldValues>(
+  // eslint-disable-next-line react/require-default-props
+  props: TextAreaFieldProps<T> & { isControlled?: boolean },
+) {
+  const { control, getValues } = useFormContext();
+  const {
+    defaultValue,
+    name,
+    labelName,
+    options,
+    containerStyle,
+    textAreaStyle,
+    isControlled = false,
+  } = props;
+  const fieldValue = getValues(name as string) || defaultValue;
+
+  return isControlled ? (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={fieldValue}
+      rules={options}
+      render={({ field }) => (
+        <TextAreaField
+          {...field}
+          containerStyle={containerStyle}
+          labelName={labelName}
+          className={textAreaStyle}
+        />
+      )}
+    />
+  ) : (
+    <TextAreaField />
+  );
 };
