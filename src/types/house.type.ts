9c85332b61b41ef2c9b3838a3c19217089e7cb95
UHa <https00200@gmail.com>
2024-06-03 13:31:11 +0900

    🚀 [Feat]: Delete recoil state & Modify house type
    - useForm의 default values로 house data를 관리하면서 recoil로 정의한 state를 사용하지 않게 되어 삭제
    - default values에 맞게 type 수정

--- src/types/house.type.ts
+++ src/types/house.type.ts
@@ -1,21 +1,19 @@
 export type HouseType = {
-  id: string | undefined;
-  created_at: string | undefined;
-  updated_at: string | undefined;
   house_img: string[];
   post_title: string;
   region: string;
   district: string;
-  house_type: 0 | 1 | 2 | 3;
-  rental_type: 0 | 1 | 2;
+  house_type: 0 | 1 | 2 | 3 | undefined;
+  rental_type: 0 | 1 | 2 | 3 | undefined;
   house_size: number;
-  room_num: 1;
+  room_num: number;
   deposit_price: number;
   monthly_price: number;
+  manage_price: number;
   house_appeal: string[];
   mates_num: number;
   term: [number, number];
-  describe: string;
+  describe?: string;
   bookmark: 0 | 1;
   visible: 0 | 1 | undefined;
   user_id: string | undefined;
