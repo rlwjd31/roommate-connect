d0e2a7d900c2bde590b18d978bbcf7b24844db01
rlwjd31 <rlwjd31@naver.com>
2024-06-12 22:43:10 +0900

    🚀 [Feat]: Apply sticky & NavLink
    - 대화목록 sticky 적용
    - 각 대화목록 NavLink 적용

--- src/components/pages/Chat.tsx
+++ src/components/pages/Chat.tsx
@@ -5,7 +5,7 @@ import Container from '@/components/atoms/Container';
 
 export default function Chat() {
   return (
-    <Container.FlexRow className="w-full">
+    <Container.FlexRow className="min-h-full w-full bg-green-100">
       <ChatList />
       <Outlet />
     </Container.FlexRow>
--- src/components/templates/ChatList.tsx
+++ src/components/templates/ChatList.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 import Container from '@/components/atoms/Container';
 import Img from '@/components/atoms/Img';
 import Typography from '@/components/atoms/Typography';
@@ -29,113 +31,183 @@ PointAlert.defaultProps = {
   typoStyle: '',
 };
 
+// ! TODO: fetch chatList ordered by latest from supabase
+// ! TODO: navigateUrl은 그냥 chatId를 이용하면 돼서 나중에는 필요 없음.
+const chats = [
+  {
+    chatId: '1',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '1',
+  },
+  {
+    chatId: '2',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '2',
+  },
+  {
+    chatId: '3',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '3',
+  },
+  {
+    chatId: '4',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '4',
+  },
+  {
+    chatId: '5',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '5',
+  },
+  {
+    chatId: '6',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '6',
+  },
+  {
+    chatId: '7',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '7',
+  },
+  {
+    chatId: '8',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '8',
+  },
+  {
+    chatId: '9',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '9',
+  },
+  {
+    chatId: '10',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '10',
+  },
+  {
+    chatId: '11',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '11',
+  },
+  {
+    chatId: '12',
+    avatarUrl:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c',
+    nickname: 'User1234',
+    latestDate: '오후 9:48',
+    content: '안녕하세요',
+    newChatCount: 2,
+    navigateUrl: '12',
+  },
+];
+
 export default function ChatList() {
   return (
-    <Container.FlexCol className="w-full max-w-[21.75rem] border-0.5 border-r-brown1 bg-brown6">
-      <Container.FlexRow className="items-center gap-2 p-6">
+    <Container.FlexCol className="w-full max-w-[21.75rem] border-r-0.5 border-r-brown1">
+      <Container.FlexRow className="sticky left-0 top-0 items-center gap-2 bg-brown6 p-6">
         <Typography.SubTitle1 className="translate-y-[1px] text-brown">
           채팅
         </Typography.SubTitle1>
         <PointAlert content={12} containerStyle="self-center" />
       </Container.FlexRow>
       {/* 친구 대화 목록 전체 container */}
-      <Container.FlexCol className="gap-2 bg-bg p-2">
-        {/* 대화 목록 1개 row */}
-        <Container.FlexRow className="items-start gap-4 rounded-xl p-3">
-          {/* shrink를 0으로 설정하지 않으면 이미지가 깨짐 */}
-          <Img
-            className="size-12 shrink-0 rounded-full"
-            src="https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c"
-          />
-          <Container.FlexCol className="w-full">
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span1 className="font-bold leading-150 text-brown">
-                User1234
-              </Typography.Span1>
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                오후 9:48
-              </Typography.Span2>
-            </Container.FlexRow>
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                안녕하세요
-              </Typography.Span2>
-              <PointAlert content={2} />
-            </Container.FlexRow>
-          </Container.FlexCol>
-        </Container.FlexRow>
-        {/* 대화 목록 1개 row */}
-        <Container.FlexRow className="items-start gap-4 rounded-xl bg-brown6 p-3">
-          {/* shrink를 0으로 설정하지 않으면 이미지가 깨짐 */}
-          <Img
-            className="size-12 shrink-0 rounded-full"
-            src="https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c"
-          />
-          <Container.FlexCol className="w-full">
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span1 className="font-bold leading-150 text-brown">
-                User1234
-              </Typography.Span1>
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                오후 9:48
-              </Typography.Span2>
-            </Container.FlexRow>
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                안녕하세요
-              </Typography.Span2>
-              <PointAlert content={2} />
-            </Container.FlexRow>
-          </Container.FlexCol>
-        </Container.FlexRow>
-        {/* 대화 목록 1개 row */}
-        <Container.FlexRow className="items-start gap-4 rounded-xl p-3">
-          {/* shrink를 0으로 설정하지 않으면 이미지가 깨짐 */}
-          <Img
-            className="size-12 shrink-0 rounded-full"
-            src="https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c"
-          />
-          <Container.FlexCol className="w-full">
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span1 className="font-bold leading-150 text-brown">
-                User1234
-              </Typography.Span1>
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                오후 9:48
-              </Typography.Span2>
-            </Container.FlexRow>
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                안녕하세요
-              </Typography.Span2>
-              <PointAlert content={2} />
-            </Container.FlexRow>
-          </Container.FlexCol>
-        </Container.FlexRow>
-        {/* 대화 목록 1개 row */}
-        <Container.FlexRow className="items-start gap-4 rounded-xl p-3">
-          {/* shrink를 0으로 설정하지 않으면 이미지가 깨짐 */}
-          <Img
-            className="size-12 shrink-0 rounded-full"
-            src="https://lh3.googleusercontent.com/a/ACg8ocK7V1m2Nt7hS2fsu-YLZgNuq1Mw-Xuree9Gyfnr47mupV9pgA=s96-c"
-          />
-          <Container.FlexCol className="w-full">
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span1 className="font-bold leading-150 text-brown">
-                User1234
-              </Typography.Span1>
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                오후 9:48
-              </Typography.Span2>
-            </Container.FlexRow>
-            <Container.FlexRow className="items-center justify-between">
-              <Typography.Span2 className="font-semibold leading-150 text-brown2">
-                안녕하세요
-              </Typography.Span2>
-              <PointAlert content={2} />
-            </Container.FlexRow>
-          </Container.FlexCol>
-        </Container.FlexRow>
+      <Container.FlexCol className="gap-2 overflow-y-auto bg-bg p-2">
+        {chats.map(
+          ({
+            avatarUrl,
+            chatId,
+            content,
+            navigateUrl,
+            nickname,
+            latestDate,
+            newChatCount,
+          }) => (
+            <NavLink
+              key={chatId}
+              to={navigateUrl}
+              className={({ isActive }) =>
+                cn(
+                  'flex items-start gap-4 rounded-xl p-3',
+                  isActive ? 'bg-brown6' : 'bg-bg',
+                )
+              }
+            >
+              {/* shrink를 0으로 설정하지 않으면 이미지가 깨짐 */}
+              <Img className="size-12 shrink-0 rounded-full" src={avatarUrl} />
+              <Container.FlexCol className="w-full">
+                <Container.FlexRow className="items-center justify-between">
+                  <Typography.Span1 className="font-bold leading-150 text-brown">
+                    {nickname}
+                  </Typography.Span1>
+                  <Typography.Span2 className="font-semibold leading-150 text-brown2">
+                    {latestDate}
+                  </Typography.Span2>
+                </Container.FlexRow>
+                <Container.FlexRow className="items-center justify-between">
+                  <Typography.Span2 className="font-semibold leading-150 text-brown2">
+                    {content}
+                  </Typography.Span2>
+                  <PointAlert content={newChatCount} />
+                </Container.FlexRow>
+              </Container.FlexCol>
+            </NavLink>
+          ),
+        )}
       </Container.FlexCol>
     </Container.FlexCol>
   );
