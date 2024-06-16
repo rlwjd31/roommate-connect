33bea3d21e53bff8c39fd473ccb43a7443676e2e
rlwjd31 <rlwjd31@naver.com>
2024-06-16 21:07:10 +0900

    🚀 [Feat]: 무한로딩 임식적 방편
    - 회원가입 로직에 필요한 부분이 무한로딩을 발생시켜 임시방편으로 rendering에 따라 실행하지 못 하게 함

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,13 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
 
 import { supabase } from '@/libs/supabaseClient';
 import Container from '@/components/atoms/Container';
 import Button from '@/components/atoms/Button';
 import Typography from '@/components/atoms/Typography';
 import { UserAtom } from '@/stores/auth.store';
+import {
+  useAuthState,
+  userAdditionalInfo,
+  useUpdateUser,
+} from '@/hooks/useSign';
 
 export default function About() {
+  const isInitial = useRef(0);
   // ! TODO 추후 수정 로그인 기능 테스트를 위한 로그아웃
   // ! Layout.template.tsx 의 onAuthStateChange 에서 navigate 동작
   // ! 현재 About 컴포넌트는 Layout.template.tsx 외부에 위치한 컴포넌트로 임시로 작성
@@ -16,10 +24,26 @@ export default function About() {
   const navigate = useNavigate();
   const onClickSignOut = async () => {
     const { error } = await supabase.auth.signOut();
-    if (error === null) setUser(null);
-    // ! TODO 제거
-    navigate('/sign/in');
+    if (error) console.error(error.message);
   };
+
+  const { updateUser } = useUpdateUser();
+  const queryClient = useQueryClient();
+  const session = useAuthState();
+
+  // ! TODO: 무한 로딩으로 인해 임시방편 error 처리
+  useEffect(() => {
+    if (isInitial.current < 3) {
+      if (session && session.user.app_metadata.provider !== 'email') {
+        const queryUserAdditionalInfo = userAdditionalInfo(session);
+        queryClient.fetchQuery(queryUserAdditionalInfo).then(data => {
+          updateUser(data);
+        });
+      }
+      isInitial.current += 1;
+    }
+  }, [session]);
+
   return (
     <Container.FlexCol className="gap-4">
       <Typography.Head1>서비스 소개 및 사용방법</Typography.Head1>
