8911c53693209a52db2443f46e0ea14f73f87208
rlwjd31 <rlwjd31@naver.com>
2024-06-19 23:12:20 +0900

    🚀 [Feat]: ProtectRouter 컴포넌트화
    - template을 children으로 받아 인증된 사용자에 대한 서비스 제공
    -

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,17 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+  useLocation,
+} from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import {
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 
 import LayoutTemplate from '@/components/templates/Layout.template';
 import ComponentTest from '@/components/pages/ComponentTest';
@@ -11,6 +24,63 @@ import About from '@/components/pages/About';
 import SignUpProfileOutro from '@/components/pages/SignUpProfileOutro';
 import Chat from '@/components/pages/Chat';
 import ChatRoom from '@/components/templates/ChatRoom';
+import { UserAtom } from '@/stores/auth.store';
+import { useAuthState } from '@/hooks/useSign';
+import { createToast } from '@/libs/toast';
+
+const nonProtectedRoutes = ['/sign/in', '/sign/up', '/component-test'];
+
+type ProtectedRouterType = {
+  children: ReactElement<{ isLogin: boolean }>;
+};
+
+function ProtectedRouter({ children }: ProtectedRouterType) {
+  // * register supabase auth listener on initial rendering
+  const session = useAuthState();
+  const location = useLocation();
+  const [showComponent, setShowComponent] = useState(false);
+  const user = useRecoilValue(UserAtom);
+  const shouldBeProtected =
+    !user && !nonProtectedRoutes.includes(location.pathname);
+
+  useEffect(() => {
+    let sleep: number | undefined;
+
+    if (shouldBeProtected) {
+      sleep = window.setTimeout(() => {
+        setShowComponent(true);
+      }, 2000);
+    } else {
+      setShowComponent(false);
+    }
+
+    return () => {
+      if (!sleep) clearTimeout(sleep);
+    };
+  }, [shouldBeProtected]);
+
+  if (shouldBeProtected) {
+    createToast('redirectToLoginPage', '💡 로그인이 필요한 페이지입니다', {
+      autoClose: 2000,
+      type: 'error',
+      isLoading: false,
+      position: 'top-right',
+    });
+
+    return showComponent ? (
+      <Navigate to="/sign/in" />
+    ) : (
+      // ! TOOD: Loading Page 나오면 대체
+      <div className="flex h-screen items-center justify-center bg-green-500">
+        Redirect to Login Page...
+      </div>
+    );
+  }
+
+  return isValidElement(children)
+    ? cloneElement(children, { isLogin: !!session })
+    : null;
+}
 
 const router = createBrowserRouter([
   {
