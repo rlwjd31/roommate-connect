35a4906562c8d5f44721032c79fe7566f0eedae6
kwondns <kwondns@gmail.com>
2024-06-24 12:20:08 +0900

    🔨 [Chore]: Merge From Dev resolve
    - dev

---
+++ src/components/templates/SignUpdatePassword.template.tsx
@@ -0,0 +1,91 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import Container from '@/components/atoms/Container';
+import FormItem from '@/components/molecules/FormItem';
+import Button from '@/components/atoms/Button';
+import Typography from '@/components/atoms/Typography';
+import { SignUpdatePassword, SignUpdatePasswordType } from '@/types/auth.type';
+import IconButton from '@/components/molecules/IconButton';
+import { useSignUpdatePassword } from '@/hooks/useSignPasswordReset';
+
+export default function SignUpdatePasswordTemplate() {
+  const [showPassword, setShowPassword] = useState(false);
+  const Form = FormProvider;
+  const form = useForm<SignUpdatePasswordType>({
+    resolver: zodResolver(SignUpdatePassword),
+  });
+  const onClickShowPassword = () => setShowPassword(prev => !prev);
+  const { updatePassword, isPending } = useSignUpdatePassword();
+  const onSubmit = (data: SignUpdatePasswordType) => {
+    updatePassword(data);
+  };
+  return (
+    <Container.FlexCol className="w-full gap-[2.5rem]">
+      <Container.FlexCol className="gap-[3.5rem]">
+        <Typography.Head2 className="text-brown">
+          비밀번호 변경
+        </Typography.Head2>
+        <Container className="w-full">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <Container.FlexCol className="gap-[1.625rem]">
+                <Container className="relative">
+                  <FormItem.TextField
+                    labelName="비밀번호"
+                    type={showPassword ? 'text' : 'password'}
+                    name="password"
+                    placeholder="비밀번호 입력"
+                    inputStyle="w-full mt-[1rem]"
+                    disabled={isPending}
+                  />
+                  <IconButton.Ghost
+                    className="absolute bottom-[44px] right-[13px] top-[53px]"
+                    iconType={showPassword ? 'visible' : 'invisible'}
+                    onClick={onClickShowPassword}
+                  />
+                </Container>
+                <Container className="relative">
+                  <FormItem.TextField
+                    labelName="비밀번호 재입력"
+                    type={showPassword ? 'text' : 'password'}
+                    name="confirmPassword"
+                    placeholder="비밀번호 입력"
+                    inputStyle="w-full mt-[1rem]"
+                    disabled={isPending}
+                  />
+                  <IconButton.Ghost
+                    className="absolute bottom-[44px] right-[13px] top-[53px]"
+                    iconType={showPassword ? 'visible' : 'invisible'}
+                    onClick={onClickShowPassword}
+                  />
+                </Container>
+              </Container.FlexCol>
+              <Button.Fill
+                type="submit"
+                className="mt-[3.25rem] w-full rounded-[10px]"
+                disabled={isPending}
+              >
+                <Typography.P3 className="mx-auto my-[1rem] text-[#F4E7DB]">
+                  확인
+                </Typography.P3>
+              </Button.Fill>
+              <Link to="/sign/in" className="w-full">
+                <Button.Outline
+                  className="mx-auto my-[1rem] w-full rounded-[10px]"
+                  disabled={isPending}
+                >
+                  <Typography.P3 className="mx-auto my-[1rem] text-brown">
+                    로그인 페이지로 이동
+                  </Typography.P3>
+                </Button.Outline>
+              </Link>
+            </form>
+          </Form>
+        </Container>
+      </Container.FlexCol>
+    </Container.FlexCol>
+  );
+}
--- src/constants/iconSVG.tsx
+++ src/constants/iconSVG.tsx
@@ -27,6 +27,8 @@ import Visible from '@/assets/icons/visible.svg?react';
 import AlarmExist from '@/assets/icons/alarm-exist.svg?react';
 import AlarmNone from '@/assets/icons/alarm-none.svg?react';
 import Avartar from '@/assets/icons/avatar.svg?react';
+import PaperClip from '@/assets/icons/paper-clip.svg?react';
+import Send from '@/assets/icons/send.svg?react';
 import Camera from '@/assets/icons/camera.svg?react';
 import Next from '@/assets/icons/next.svg?react';
 import Prev from '@/assets/icons/prev.svg?react';
@@ -60,6 +62,8 @@ const IconSVG: { [key in IconType]: ReactNode } = {
   'alarm-exist': <AlarmExist />,
   'alarm-none': <AlarmNone />,
   avartar: <Avartar />,
+  'paper-clip': <PaperClip />,
+  send: <Send />,
   camera: <Camera />,
   prev: <Prev />,
   next: <Next />,
