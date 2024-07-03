c32ea87c36d87ed9ba92a090476c9af4512b0441
rlwjd31 <rlwjd31@naver.com>
2024-07-03 17:42:49 +0900

    🔨[Chore]: modify console lint
    - warn, error에 대해서 허용

--- src/hooks/useSign.ts
+++ src/hooks/useSign.ts
@@ -1,5 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import {
   AuthError,
   AuthResponse,
