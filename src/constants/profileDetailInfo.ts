168673b522abe934d3ac5460d474142c5b2fecfd
kimcode <sophy0219@naver.com>
2024-07-21 19:14:34 +0900

    ♻️ [Refactor]: 변수명 통일 및 추가
    -  값 변형을 위한 변수명 통일
    - mateNumInfo 추가

--- src/constants/profileDetailInfo.ts
+++ src/constants/profileDetailInfo.ts
@@ -1,73 +1,91 @@
-//! TODO: default value 로 변수명 변경
-export const defaultRentalTypes = {
+export const rentalTypesInfo = {
   0: '상관없음',
   1: '월세',
   2: '전세',
   3: '반전세',
 } as const;
 
-export const defaultHouseTypes = {
-  0: {
-    icon: 'studio-officetel',
-    houseInfo: '원룸/오피스텔',
-  },
-  1: {
-    icon: 'villa-townhouse',
-    houseInfo: '빌라/연립',
-  },
-  2: {
-    icon: 'apartment',
-    houseInfo: '아파트',
-  },
-  3: {
-    icon: 'single-family-house',
-    houseInfo: '단독주택',
-  },
-};
-
-export const defaultGenderTypes = {
-  0: {
-    icon: 'mini-gender-intersex',
-    sex: '상관없음',
-  },
-  1: {
-    icon: 'mini-male',
-    sex: '남성',
-  },
-  2: {
-    icon: 'mini-female',
-    sex: '여성',
-  },
-} as const;
-
-export const defaultSmokingTypes = {
-  true: {
-    icon: 'mini-smoke',
-    smokeInfo: '흡연자',
-  },
-  false: {
-    icon: 'mini-none-smoke',
-    smokeInfo: '비흡연자',
-  },
-} as const;
-
-export const defaultPetTypes = {
-  0: {
-    icon: 'mini-dont-mind-pet',
-    petInfo: '반려동물 상관없어요',
-  },
-  1: {
-    icon: 'mini-pet-lover',
-    petInfo: '반려동물 키워요',
-  },
-  2: {
-    icon: 'mini-none-pet-lover',
-    perInfo: '반려동물 NO',
-  },
-} as const;
-
-export const defaultFloorTypes = {
+export const floorInfo = {
   0: '지하',
   1: '반지하',
   2: '지상',
 } as const;
+
+export const houseTypesInfo = {
+  0: {
+    icon: 'studio-officetel',
+    text: '원룸/오피스텔',
+  },
+  1: {
+    icon: 'villa-townhouse',
+    text: '빌라/연립',
+  },
+  2: {
+    icon: 'apartment',
+    text: '아파트',
+  },
+  3: {
+    icon: 'single-family-house',
+    text: '단독주택',
+  },
+} as const;
+
+export const genderInfo = {
+  0: {
+    icon: 'icon-gender-free',
+    text: '상관없음',
+  },
+  1: {
+    icon: 'icon-male',
+    text: '남성',
+  },
+  2: {
+    icon: 'icon-female',
+    text: '여성',
+  },
+} as const;
+
+export const smokingInfo = {
+  true: {
+    icon: 'mini-smoke',
+    text: '흡연자',
+  },
+  false: {
+    icon: 'mini-none-smoke',
+    text: '비흡연자',
+  },
+} as const;
+
+export const petInfo = {
+  0: {
+    icon: 'pet-circle',
+    text: '반려동물 상관없어요',
+  },
+  1: {
+    icon: 'pet-heart',
+    text: '반려동물 키워요',
+  },
+  2: {
+    icon: 'pet-hate',
+    text: '반려동물 NO',
+  },
+} as const;
+
+export const mateNumInfo = {
+  0: {
+    icon: 'dont-mind-people',
+    text: '상관없음',
+  },
+  1: {
+    icon: 'one-person',
+    text: '1명',
+  },
+  2: {
+    icon: 'two-people',
+    text: '2명',
+  },
+  3: {
+    icon: 'three-people',
+    text: '3명이상',
+  },
+};
