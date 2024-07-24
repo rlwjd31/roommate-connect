a415657b716550d0ef258daa7ef4a9baf653e228
kwondns <kwondns@gmail.com>
2024-07-25 01:51:42 +0900

    🚀 [Feat]: MyBookmark Page 제작
    - useQueries를 사용
    - 북마크된 하우스 리스트 9개 단위, 북마크 하우스 총 개수를 패칭
    - 패칭 후 MyBookmarkTemplate props로 전달

--- src/components/pages/MyBookmark.tsx
+++ src/components/pages/MyBookmark.tsx
@@ -1,13 +1,41 @@
-import { useRecoilValue } from 'recoil';
-import { useQuery } from '@tanstack/react-query';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { useQueries } from '@tanstack/react-query';
 
 import MyBookmarkTemplate from '@/components/templates/MyBookmark.template';
 import { UserAtom } from '@/stores/auth.store';
-import { useMyBookmarkList } from '@/hooks/useMyBookmark';
+import {
+  HouseBookmarkType,
+  useMyBookmarkHouseCount,
+  useMyBookmarkHouseList,
+} from '@/hooks/useMyBookmark';
+import {
+  BookmarkHouseFilterAtom,
+  BookmarkPageAtom,
+} from '@/stores/bookmark.store';
 
 export default function MyBookmark() {
   const user = useRecoilValue(UserAtom);
-  const { data } = useQuery(useMyBookmarkList(user));
-  console.log(data);
-  return <MyBookmarkTemplate />;
+  const pageState = useRecoilState(BookmarkPageAtom);
+  const houseFilter = useRecoilValue(BookmarkHouseFilterAtom);
+
+  // TODO Lounge, Article을 생성 후 변경
+  const data = useQueries({
+    queries: [
+      useMyBookmarkHouseList(user, pageState[0], houseFilter),
+      useMyBookmarkHouseCount(user, houseFilter),
+    ],
+  });
+
+  const [houseListData, houseCountData] = data;
+
+  const { data: house } = houseListData;
+  const { data: houseCount } = houseCountData;
+
+  return (
+    <MyBookmarkTemplate
+      house={house?.data as HouseBookmarkType[]}
+      houseCount={houseCount?.count as number}
+      pageState={pageState}
+    />
+  );
 }
