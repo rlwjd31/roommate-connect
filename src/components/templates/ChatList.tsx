4800c3c9b13c623431e0c9177e6b2037779cf5c6
rlwjd31 <rlwjd31@naver.com>
2024-07-26 08:53:06 +0900

    🚀 [Feat]: chatList 페이지에 필요한 데이터 react query 적용
    - useQueries를 이용하여 기존의 Waterfall fetching을 막고 병렬적으로
      fetch를 수행

--- src/components/templates/ChatList.tsx
+++ src/components/templates/ChatList.tsx
@@ -1,19 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import Container from '@/components/atoms/Container';
 import Typography from '@/components/atoms/Typography';
 import cn from '@/libs/cn';
 import Avatar from '@/components/atoms/Avatar';
-import { supabase } from '@/libs/supabaseClient';
-import SupabaseCustomError from '@/libs/supabaseCustomError';
 import { UserAtom } from '@/stores/auth.store';
 import { Tables } from '@/types/supabase';
 import { formatDateByCountry, isToday } from '@/libs/dateUtils';
 import {
-  fetchLastReadDate,
-  fetchUnReadMessagesCount,
+  useChatRoomListPageData,
   useUnReadMessageCount,
 } from '@/hooks/useChat';
 
@@ -58,78 +54,13 @@ PointAlert.defaultProps = {
 const chatRoomId = '2c818fc5-8b49-44ff-8713-b4ece60c36d5';
 
 export default function ChatList() {
-  const [chatListState, setChatListState] = useState<ChatListState[]>([]);
   const userInfo = useRecoilValue(UserAtom);
   const unReadMessageCount = useUnReadMessageCount(
     userInfo?.id ?? '',
     chatRoomId,
   );
-
-  console.log('unReadMessageCount', unReadMessageCount);
-
-  useEffect(() => {
-    if (userInfo) {
-      (async () => {
-        // ! PostgreSQL에서 배열 타입의 컬럼에 대해 외래 키 제약 조건을 설정하는 것은 지원되지 않아
-        // ! join을 할 수 없고 따로따로 fetch를 날리는 것으로 해결
-        const chatRoomListResponse = await supabase
-          .from('chat_room')
-          .select('*')
-          .contains('users', [userInfo.id])
-          .order('last_message_date', { ascending: false });
-
-        // * chatRoomList가 없을 때는 null로 들어옴
-        if (chatRoomListResponse.error && !chatRoomListResponse.data) {
-          const supabaseError = new SupabaseCustomError(
-            chatRoomListResponse.error,
-            chatRoomListResponse.status,
-          );
-          console.error({ supabaseError });
-          throw supabaseError;
-        }
-
-        const chatRoomListPageData = await Promise.all(
-          chatRoomListResponse.data.map(async chatRoomInfo => {
-            const {
-              id: chatId,
-              last_message: lastMessage,
-              last_message_date: lastMessageDate,
-              users: userIds,
-            } = chatRoomInfo;
-
-            const chatPartnerId = userIds?.filter(
-              userId => userId !== userInfo?.id,
-            )[0];
-
-            if (!chatPartnerId)
-              throw new Error(`couldn't find chat partner id`);
-
-            const chatPartnerResponse = await supabase
-              .from('user')
-              .select('*')
-              .eq('id', chatPartnerId)
-              .single();
-
-            // TODO: chatPartnerResponse error 처리
-            if (!chatPartnerResponse.data && chatPartnerResponse.error)
-              throw new Error(`Couldn't find chat partner info`);
-
-            return {
-              chatId,
-              lastMessage,
-              lastMessageDate,
-              userIds,
-              chatPartnerInfo: chatPartnerResponse.data,
-            };
-          }),
-        );
-        // TODO: 안 읽은 message count 기능
-        setChatListState(chatRoomListPageData);
-        return chatRoomListPageData;
-      })();
-    }
-  }, [userInfo]);
-
+  const chatListPageData = useChatRoomListPageData(userInfo?.id ?? '')!;
+  console.log('chatListPageData', chatListPageData);
   return (
     <Container.FlexCol className="w-full max-w-[21.75rem] border-r-0.5 border-r-brown1">
       <Container.FlexRow className="sticky left-0 top-0 items-center gap-2 bg-brown6 p-6">
@@ -140,7 +71,7 @@ export default function ChatList() {
       </Container.FlexRow>
       {/* 친구 대화 목록 전체 container */}
       <Container.FlexCol className="gap-2 overflow-y-auto bg-bg p-2">
-        {chatListState.map(
+        {chatListPageData.map(
           ({
             chatPartnerInfo: { avatar, nickname },
             chatId,
--- src/hooks/useChat.tsx
+++ src/hooks/useChat.tsx
@@ -1,7 +1,12 @@
-import { QueryFunctionContext, useQuery } from '@tanstack/react-query';
+import {
+  QueryFunctionContext,
+  useQueries,
+  useQuery,
+} from '@tanstack/react-query';
 
 import { supabase } from '@/libs/supabaseClient';
 import SupabaseCustomError from '@/libs/supabaseCustomError';
+import { Tables } from '@/types/supabase';
 
 /**
  * [queryKey, userId, chatRoomId]
@@ -11,6 +16,14 @@ type LastReadDateProps = [string, string, string];
  * [queryKey, chatRoomId, lastReadDate]
  */
 type UnReadMessagesProps = [string, string, string];
+/**
+ * [queryKey, userId]
+ */
+type ChatRoomListProps = [string, string];
+/**
+ * [queryKey, chatPartnerId]
+ */
+type ChatPartnerInfoProps = [string, string];
 
 // TODO: suspense와 ErrorBoundary사용을 위해 throwOnError & suspense option활성화
 const fetchLastReadDate = async ({
@@ -52,6 +65,41 @@ const fetchUnReadMessagesCount = async ({
   return data.length;
 };
 
+const fetchChatRoomList = async ({
+  queryKey,
+}: QueryFunctionContext<ChatRoomListProps>) => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [_, userId] = queryKey;
+  const { data, error, status } = await supabase
+    .from('chat_room')
+    .select('*')
+    .contains('users', [userId])
+    .order('last_message_date', { ascending: false });
+
+  // * chatRoomList가 없을 때는 null로 들어옴
+  if (error && !data) {
+    throw new SupabaseCustomError(error, status);
+  }
+
+  return data;
+};
+
+const fetchChatPartnerInfo = async ({
+  queryKey,
+}: QueryFunctionContext<ChatPartnerInfoProps>) => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [_, chatPartnerId] = queryKey;
+  const { data, error, status } = await supabase
+    .from('user')
+    .select('*')
+    .eq('id', chatPartnerId)
+    .single();
+
+  if (!data && error) throw new SupabaseCustomError(error, status);
+
+  return data;
+};
+
 // db join이 table구성 상 되지 않는 관계로 dependent query로 구성
 // TODO: 추후 db 바꿀 시 join으로 수정
 const useUnReadMessageCount = (userId: string, chatRoomId: string) => {
@@ -70,8 +118,54 @@ const useUnReadMessageCount = (userId: string, chatRoomId: string) => {
   return unReadMessagesCount;
 };
 
-export {
-  fetchLastReadDate,
-  fetchUnReadMessagesCount,
-  useUnReadMessageCount,
+const useChatRoomListPageData = (userId: string) => {
+  const { data: chatRoomList } = useQuery({
+    queryKey: ['chatRoomList', userId],
+    queryFn: fetchChatRoomList,
+    enabled: !!userId,
+  });
+
+  const chatRoomListPageData = useQueries({
+    queries: chatRoomList
+      ? chatRoomList.map(chatRoomInfo => {
+          const {
+            id: chatId,
+            last_message: lastMessage,
+            last_message_date: lastMessageDate,
+            users: userIds,
+          } = chatRoomInfo;
+
+          const chatPartnerId = userIds?.filter(
+            eachUserId => eachUserId !== userId,
+          )[0];
+
+          if (!chatPartnerId) throw new Error(`couldn't find chat partner id`);
+
+          return {
+            queryKey: ['chatPartnerInfo', chatPartnerId] as [string, string],
+            queryFn: fetchChatPartnerInfo,
+            // * combine에서 하려 했지만, chatId, lastMessage 등 값을 scope 때문에 참조할 수 없어 select를 사용
+            select: (data: Tables<'user'>) => ({
+              chatId,
+              lastMessage,
+              lastMessageDate,
+              userIds,
+              chatPartnerInfo: { ...data },
+            }),
+            enabled: !!chatPartnerId,
+          };
+        })
+      : [],
+    // ? useQueries의 반환값이 각 배열 요소에 useQuery의 return 값이므로 combine을 통해 필요한 data로만 치환함.
+    combine: results => ({
+      // ! chatListPageData가 undefined[]와 같은 type으로도 추론되어 filter를 적용
+      data: results
+        .map(result => result.data)
+        .filter(data => data !== undefined),
+    }),
+  });
+
+  return chatRoomListPageData.data;
 };
+
+export { useUnReadMessageCount, useChatRoomListPageData };
