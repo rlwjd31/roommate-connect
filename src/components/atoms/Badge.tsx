969b631759b81d7c745e4cabe11f6420b1bb33e9
kwondns <kwondns@gmail.com>
2024-07-25 20:38:49 +0900

    🔨 [Chore]: Patch 181f4448
    - Badge 스타일 관련 패치

--- src/components/atoms/Badge.tsx
+++ src/components/atoms/Badge.tsx
@@ -5,9 +5,12 @@ import cn from '@/libs/cn';
 export type BadgeProps = {
   children: React.ReactNode;
   className?: string;
+  focus?: boolean;
+  active?: boolean;
+  hover?: boolean;
 };
 
-type BadgeType = 'Fill' | 'Outline';
+export type BadgeType = 'Fill' | 'Outline';
 
 type BadgeComponentProps = {
   [key in BadgeType]: (props: BadgeProps) => React.ReactNode;
@@ -15,26 +18,44 @@ type BadgeComponentProps = {
 const badgeType: { type: BadgeType; defaultClassName: string }[] = [
   {
     type: 'Fill',
-    defaultClassName:
-      'group flex items-center text-bg bg-brown hover:bg-brown1 hover:shadow-[0_4px_4px_0] hover:shadow-brown/25 focus:bg-brown focus:text-bg focus:outline-subColor1 focus:shadow-[0_4px_4px_0] focus:outline focus:outline-[3px] focus:shadow-brown/25',
+    defaultClassName: 'group flex items-center text-bg bg-brown ',
   },
   {
     type: 'Outline',
     defaultClassName:
-      'group flex bg-bg items-center text-brown border-brown border hover:bg-hover-outline hover:shadow-[0_4px_4px_0] hover:shadow-brown/25  focus:outline-[3px] focus:outline focus:border-transparent focus:outline-subColor1 focus:shadow-[0_4px_4px_0] focus:shadow-brown/25',
+      'group flex bg-bg items-center text-brown border-brown border ',
   },
 ];
 const Badge = {} as BadgeComponentProps;
 badgeType.forEach(({ type, defaultClassName }) => {
-  Badge[type] = ({ children, className, ...others }: BadgeProps) =>
-    React.createElement(
+  Badge[type] = ({
+    children,
+    className,
+    focus = true,
+    active = true,
+    hover = true,
+    ...others
+  }: BadgeProps) => {
+    const focusClass = focus
+      ? 'focus:text-bg focus:bg-brown focus:outline-subColor1 focus:shadow-[0_4px_4px_0] focus:outline focus:outline-[3px] focus:shadow-brown/25'
+      : '';
+    const activeClass = active
+      ? 'active:bg-active-fill active:shadow-none'
+      : '';
+    const hoverClass =
+      type === 'Fill'
+        ? 'hover:bg-brown1 hover:shadow-[0_4px_4px_0] hover:shadow-brown/25'
+        : 'hover:bg-hover-outline hover:shadow-[0_4px_4px_0] hover:shadow-brown/25';
+
+    return React.createElement(
       'div',
       {
-        className: `${defaultClassName} ${cn(className)} `,
+        className: `${defaultClassName} ${cn(className)} ${focusClass} ${activeClass} ${hover ? hoverClass : ''}`,
         ...others,
       },
       children,
     );
+  };
 });
 
 export default Badge;
