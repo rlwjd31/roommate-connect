07df878f4b574b1b3c8003f9bd0bc564fad67aec
kwondns <kwondns@gmail.com>
2024-07-25 20:48:52 +0900

    💄 [Design]: Badge patch 후 적용
    - MyActivity 페이지의 Badge hover 관련 효과 제거

--- src/components/templates/MyActivity.template.tsx
+++ src/components/templates/MyActivity.template.tsx
@@ -92,12 +92,17 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
                 유형
               </Typography.P3>
               {/* TODO Badge 최신화 후 hover효과 제거 */}
-              <Badge.Outline className="mr-2">
+              <Badge.Outline
+                className="mr-2"
+                hover={false}
+                active={false}
+                focus={false}
+              >
                 <Typography.P2>
                   {houseTypesInfo[user.user_looking_house.type].text}
                 </Typography.P2>
               </Badge.Outline>
-              <Badge.Outline>
+              <Badge.Outline hover={false} active={false} focus={false}>
                 <Typography.P2>
                   {rentalTypesInfo[user.user_looking_house.rental_type]}
                 </Typography.P2>
@@ -108,7 +113,13 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
                 위치
               </Typography.P3>
               {user.user_looking_house.regions.map(region => (
-                <Badge.Outline key={region} className="mr-2">
+                <Badge.Outline
+                  key={region}
+                  hover={false}
+                  active={false}
+                  focus={false}
+                  className="mr-2"
+                >
                   <Typography.P2>{region}</Typography.P2>
                 </Badge.Outline>
               ))}
@@ -117,7 +128,12 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
               <Typography.P3 className="pr-[1.25rem] text-brown">
                 기간
               </Typography.P3>
-              <Badge.Outline className="mr-2">
+              <Badge.Outline
+                className="mr-2"
+                hover={false}
+                active={false}
+                focus={false}
+              >
                 <Typography.P2>{`최소 ${generateUnitByTerm(user.user_looking_house.term[0], 25)}부터 최대 ${generateUnitByTerm(user.user_looking_house.term[1], 25)} 까지`}</Typography.P2>
               </Badge.Outline>
             </BadgeContainer>
@@ -130,11 +146,21 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
           </Container.FlexRow>
           <BadgeContainer>
             <Typography.P3 className="pr-3 text-brown">보증금</Typography.P3>
-            <Badge.Outline className="!mr-5">
+            <Badge.Outline
+              className="!mr-5"
+              hover={false}
+              active={false}
+              focus={false}
+            >
               <Typography.P2>{`${generateUnitByPrice(user.user_looking_house.deposit_price[0], 10001)} ~ ${generateUnitByPrice(user.user_looking_house.deposit_price[1], 10001)}`}</Typography.P2>
             </Badge.Outline>
             <Typography.P3 className="pr-4 text-brown">월세</Typography.P3>
-            <Badge.Outline className="!mr-0">
+            <Badge.Outline
+              className="!mr-0"
+              hover={false}
+              active={false}
+              focus={false}
+            >
               <Typography.P2>{`${generateUnitByPrice(user.user_looking_house.monthly_rental_price[0], 501)} ~ ${generateUnitByPrice(user.user_looking_house.monthly_rental_price[1], 501)}`}</Typography.P2>
             </Badge.Outline>
           </BadgeContainer>
@@ -170,7 +196,12 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
               </Typography.SubTitle2>
               <BadgeContainer className="pb-[3.25rem]">
                 {user.user_lifestyle.appeals.map(value => (
-                  <Badge.Outline key={value}>
+                  <Badge.Outline
+                    key={value}
+                    hover={false}
+                    active={false}
+                    focus={false}
+                  >
                     <Typography.P2>{value}</Typography.P2>
                   </Badge.Outline>
                 ))}
@@ -200,7 +231,12 @@ export default function MyActivityTemplate(props: MyActivityTemplateProps) {
               </BadgeContainer>
               <BadgeContainer>
                 {user.user_mate_style.mate_appeals.map(value => (
-                  <Badge.Outline key={value}>
+                  <Badge.Outline
+                    key={value}
+                    hover={false}
+                    active={false}
+                    focus={false}
+                  >
                     <Typography.P2>{value}</Typography.P2>
                   </Badge.Outline>
                 ))}
