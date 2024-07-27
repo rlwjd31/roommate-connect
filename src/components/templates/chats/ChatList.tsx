8238df4b3ec637e1cd1b90ec83882a3e9cbc0c79
rlwjd31 <rlwjd31@naver.com>
2024-07-28 02:59:51 +0900

    ♻️ [Refactor]: NewChatCountCircle Componentization
    NewChatCountCircle 컴포넌트화

--- src/components/templates/chats/ChatList.tsx
+++ src/components/templates/chats/ChatList.tsx
@@ -11,36 +11,7 @@ import { UserAtom } from '@/stores/auth.store';
 import { formatDateByCountry, isToday } from '@/libs/dateUtils';
 import { useChatRoomListPageData, useUpdateLastRead } from '@/hooks/useChat';
 import { supabase } from '@/libs/supabaseClient';
-
-type NewChatCountCircleType = {
-  content: number;
-  containerStyle?: string;
-  typoStyle?: string;
-};
-
-export function NewChatCountCircle({
-  content,
-  containerStyle,
-  typoStyle,
-}: NewChatCountCircleType) {
-  return content > 0 ? (
-    <Container.FlexRow
-      className={cn(
-        'w-fit items-center justify-center rounded-full bg-point pl-2 pr-[7px] pt-[4px] pb-[5px]',
-        containerStyle,
-      )}
-    >
-      <Typography.Span2 className={cn('font-semibold text-bg', typoStyle)}>
-        {content}
-      </Typography.Span2>
-    </Container.FlexRow>
-  ) : null;
-}
-
-NewChatCountCircle.defaultProps = {
-  containerStyle: '',
-  typoStyle: '',
-};
+import NewChatCountCircle from '@/components/templates/chats/\bNewChatCountCircle';
 
 export default function ChatList() {
   const userInfo = useRecoilValue(UserAtom);
