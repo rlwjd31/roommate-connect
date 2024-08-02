f99dc4790d74791808db8d35ffc62ce4d2fa8c4f
kwondns <kwondns@gmail.com>
2024-08-02 16:14:27 +0900

    🚀 [Feat]: useUpdateProfile 작성
    - SignUpProfileType과 같은 payload, 수정을 위한 유저의 id 2가지가 필요
    - 해당 데이터를 user_lifestyle, user_mate_style, user_looking_house
    - 3가지의 테이블에 맞는 데이터로 업데이트 수행
    - Promise.All을 통해 동시 수행
    - 그 결과 중 하나라도 error가 있을 경우 에러 발생
    - mutation 성공 시 closeProfileModifyModal로 모달 종료

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,7 +1,10 @@
-import { queryOptions } from '@tanstack/react-query';
+import { queryOptions, useMutation } from '@tanstack/react-query';
 
 import { supabase } from '@/libs/supabaseClient';
 import { UserType } from '@/types/auth.type';
+import { RegionUnion, SignUpProfileType } from '@/types/signUp.type';
+import { createToast, errorToast, successToast } from '@/libs/toast';
+import useModal from '@/hooks/useModal';
 
 export type UserInfoType = {
   avatar: string;
@@ -16,7 +19,7 @@ export type UserInfoType = {
 type UserLifeStyleType = {
   pet: 0 | 1 | 2;
   appeals: string[];
-  smoking: 'true' | 'false';
+  smoking: boolean;
 };
 
 type UserLookingHouseType = {
@@ -25,7 +28,7 @@ type UserLookingHouseType = {
   rental_type: 0 | 1 | 2;
   deposit_price: [number, number];
   monthly_rental_price: [number, number];
-  regions: string[];
+  regions: RegionUnion[];
 };
 
 type UserMateStyleType = {
@@ -48,3 +51,66 @@ export const userInfoQuery = (user: UserType | null) =>
         .single(),
     enabled: !!user,
   });
+
+export const useUpdateProfile = () => {
+  const { closeModal: closeProfileModifyModal } = useModal('ProfileModify');
+
+  const { mutate: updateProfile, isPending } = useMutation({
+    mutationFn: async (payload: SignUpProfileType & { id: string }) => {
+      const {
+        id,
+        smoking,
+        pet,
+        appeals,
+        mate_appeals,
+        mate_number,
+        mate_gender,
+        monthly_rental_price,
+        deposit_price,
+        rental_type,
+        type,
+        term,
+        regions,
+      } = payload;
+      const userLifeStyle = {
+        smoking,
+        pet,
+        appeals,
+      };
+      const userMateStyle = {
+        mate_gender,
+        mate_number,
+        mate_appeals,
+      };
+      const userLookingHouse = {
+        type,
+        rental_type,
+        monthly_rental_price,
+        deposit_price,
+        term,
+        regions,
+      };
+
+      const response = await Promise.all([
+        supabase.from('user_lifestyle').update(userLifeStyle).eq('id', id),
+        supabase
+          .from('user_looking_house')
+          .update(userLookingHouse)
+          .eq('id', id),
+        supabase.from('user_mate_style').update(userMateStyle).eq('id', id),
+      ]);
+      response.forEach(value => {
+        if (value.error) throw new Error(value.error.message);
+      });
+    },
+    onMutate: () =>
+      createToast('update-user-profile', '유저의 정보를 업데이트 중입니다...'),
+    onSuccess: () => {
+      successToast('update-user-profile', '유저의 정보를 업데이트했습니다.');
+      closeProfileModifyModal();
+    },
+    onError: () =>
+      errorToast('update-user-profile', '유저 정보 업데이트에 실패했습니다.'),
+  });
+  return { updateProfile, isPending };
+};
