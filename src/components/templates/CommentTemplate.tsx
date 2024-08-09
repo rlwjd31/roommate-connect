2ffd77c6eba24fc10c7b7962e2f3ab38d75278f0
kimcode <sophy0219@naver.com>
2024-08-09 14:02:52 +0900

    🚀 [Feat]: useQueries 적용
    - house_comment fetching useEffect 제거
    - useQueries로 houseComment data fetching
    - fetch 값 CommentReplyList로 전달
    - 추후 houseDetailPage 로 분리 예정

--- src/components/templates/CommentTemplate.tsx
+++ src/components/templates/CommentTemplate.tsx
@@ -1,58 +1,36 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import Container from '@/components/atoms/Container';
 import Typography from '@/components/atoms/Typography';
-import { supabase } from '@/libs/supabaseClient';
-import { CommentType } from '@/types/houseComment.type';
 import CommentReplyList from '@/components/organisms/CommentReplyList';
 import CommentRegister from '@/components/molecules/CommentRegister';
+import { houseCommentQuery } from '@/hooks/useCommentReply';
+import { CommentType } from '@/types/houseComment.type';
 
 function CommentTemplate() {
   const { houseId } = useParams();
-  const [comments, setComments] = useState<CommentType[] | null>(null);
-  const [countComment, setCountComment] = useState<number>(0);
-  const fetchData = async () => {
-    const {
-      data: comment,
-      error,
-      count,
-    } = await supabase
-      .from('house_comment')
-      .select('*, house_reply(*,user(nickname,avatar)),user(nickname,avatar)', {
-        count: 'exact',
-      })
-      .eq('house_id', houseId ?? '');
+  const { data: comments } = useQuery(houseCommentQuery(houseId));
 
-    console.log('comment', comment);
-    if (error) {
-      console.log(error.message);
-    }
-    return { comment, count };
-  };
-
-  useEffect(() => {
-    (async () => {
-      const houseComment = await fetchData();
-      setComments(houseComment.comment);
-      setCountComment(houseComment.count as number);
-      console.log('a', houseComment);
-    })();
-  }, [houseId]);
-
-  if (!comments) return <h1>?</h1>;
   return (
     <Container.FlexCol>
       <Container.FlexCol className="gap-8 py-8">
         <Typography.SubTitle1 className="text-brown">
-          댓글 {countComment}개
+          댓글 {comments?.count}개
         </Typography.SubTitle1>
-        <CommentRegister />
+        <CommentRegister
+          topId={houseId as string}
+          methodType="insert"
+          isReply={false}
+        />
       </Container.FlexCol>
 
       {comments &&
-        comments.map(comment => (
-          <CommentReplyList key={comment.id} comment={comment} />
+        comments?.data?.map(comment => (
+          <CommentReplyList
+            key={comment?.id}
+            comment={comment as unknown as CommentType}
+          />
         ))}
     </Container.FlexCol>
   );
