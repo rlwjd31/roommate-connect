b4dadd4798f91c1b41d203201a0d939720fcc3ea
kimcode <sophy0219@naver.com>
2024-08-09 13:59:59 +0900

    🚀 [Feat]: useCommentReply hooks 생성 및 타입 정의
    - <useCommentReply>
    - houseComment data fetch queryOptions 정의
    - comment  insert / update / delete 에 대한 methodType 정의
    - house_comment, house_reply 변경 요청 MutationFn 생성
    - onSuccess 시 comment 데이터가 최신화가 필요하다는 것을 알려줌
    - queryOptions에 정의한 querykeys를 통해 최신화
    - <houseComment.type>
    - CommentForm 에 content 부분 zod 검증 추가

--- src/types/houseComment.type.ts
+++ src/types/houseComment.type.ts
@@ -1,3 +1,14 @@
+import { z } from 'zod';
+
+export const CommentForm = z.object({
+  content: z
+    .string()
+    .min(2, { message: '최소 2글자 이상 작성해주세요' })
+    .max(200, { message: '최대 200글자까지 가능합니다.' }),
+});
+
+export type CommentFormType = z.infer<typeof CommentForm>;
+
 export type CommentType = {
   content: string;
   created_at: string;
