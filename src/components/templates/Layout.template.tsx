8e797708307e7b2bc86cd7cc0054d3a6f3bd6af7
rlwjd31 <rlwjd31@naver.com>
2024-08-29 18:12:47 +0900

    🚀 [Feat]: 동적 header height 해결 & font 우선순위 재 정렬
    - header height가 동적 스타일에 의해 적용되지 않아 그대로 넣어 해결
    - 모든 영어가 noto sans가 아닌 특정 부분에만 Myanmar font가 적용되므로 재 정렬

--- src/components/templates/Layout.template.tsx
+++ src/components/templates/Layout.template.tsx
@@ -8,15 +8,13 @@ export default function LayoutTemplate() {
   // * supabase authListener를 등록함과 동시에 isLogin상태를 가져오기 위함
   const [session] = useAuthState();
 
-  const headerHeight = '7.75rem';
-
   return (
     <>
       {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-      <Header isLogin={!!session} className={`min-h-[${headerHeight}]`} />
+      <Header isLogin={!!session} className="min-h-[7.75rem]" />
       <main
         className={cn(
-          `flex bg-bg flex-col relative max-w-[79rem] px-8 mx-auto h-screen pt-[${headerHeight}]`,
+          `flex bg-bg flex-col relative max-w-[79rem] px-8 mx-auto h-screen pt-[7.75rem]`,
         )}
       >
         <Outlet />
