99c23e4269ef724bc1254c9a213873ae13aef798
rlwjd31 <rlwjd31@naver.com>
2024-09-04 21:30:53 +0900

    ♻️  [Refactor]: Move Feature of Header from Layout.template.tsx to Header.tsx
    - header와 관련된 기능을 Header.tsx로 옮겨 책임 분리를 진행 함.

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,56 +1,84 @@
 import { ComponentProps, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useLocation } from 'react-router-dom';
 
-import { routePaths } from '@/constants/route';
+import { routeHeaderInfo, routePaths } from '@/constants/route';
 import Container from '@/components/atoms/Container';
 import Icon from '@/components/atoms/Icon';
 import Link from '@/components/atoms/Link';
 import cn from '@/libs/cn';
 import { UserAtom } from '@/stores/auth.store';
 import { GNB, UserMenu } from '@/components/organisms/header';
+import isRoutePathMatched from '@/libs/isPathMatched';
+import useIsOverSTabletBreakpoint from '@/hooks/useIsOverSTabletBreakpoint';
 
 type Props = ComponentProps<'header'> & {
   className?: string;
   isLogin: boolean;
-  exist: {
-    logo?: boolean;
-    gnb?: boolean;
-    userMenu?: boolean;
-  };
 };
 
-export default function Header({
-  className,
-  isLogin,
-  exist,
-  ...others
-}: Props) {
-  // ! TODO: 알람기능 추가시 바꿔야 함
+export default function Header({ className, isLogin, ...others }: Props) {
   const [hasNewAlarm] = useState(false);
   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   const [isUserMenuActive, _setIsUserMenuActive] = useState(false);
   const user = useRecoilValue(UserAtom);
+  const location = useLocation();
+  const [isOverSTabletBreakPoint] = useIsOverSTabletBreakpoint();
+
+  const commonHeaderStyle = 'flex h-[8rem] items-center fixed w-screen z-50';
+
+  const getHeaderConfig = (locationPathName: string) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const routePathType of Object.keys(routePaths)) {
+      if (
+        isRoutePathMatched(
+          locationPathName,
+          routePathType as keyof typeof routePaths,
+        )
+      )
+        return routeHeaderInfo[routePathType as keyof typeof routePaths];
+    }
+
+    return routeHeaderInfo.default;
+  };
+
+  const headerConfig = getHeaderConfig(location.pathname);
 
   return (
-    <header className={cn('top-0 left-0 bg-bg', className)} {...others}>
-      <Container.FlexRow className="mx-auto w-full max-w-[79rem] items-center justify-between px-8">
-        {exist.logo && (
-          <Link to={routePaths.root}>
-            <Icon type="logo" className="h-[3rem] w-[5.9375rem]" />
-          </Link>
-        )}
-
-        {exist.gnb && <GNB />}
-        {exist.userMenu && (
-          <UserMenu
-            user={user}
-            isLogin={isLogin}
-            hasNewAlarm={hasNewAlarm}
-            isUserMenuActive={isUserMenuActive}
+    <>
+      {(isOverSTabletBreakPoint || headerConfig.isHeaderExistUnderSTablet) && (
+        <header
+          className={cn(commonHeaderStyle, 'top-0 left-0 bg-bg', className)}
+          {...others}
+        >
+          <Container.FlexRow className="mx-auto w-full max-w-[79rem] items-center justify-between px-8">
+            {headerConfig.logo && (
+              <Link to={routePaths.root}>
+                <Icon type="logo" className="h-[3rem] w-[5.9375rem]" />
+              </Link>
+            )}
+            {headerConfig.gnb && <GNB />}
+            {headerConfig.userMenu && (
+              <UserMenu
+                user={user}
+                isLogin={isLogin}
+                hasNewAlarm={hasNewAlarm}
+                isUserMenuActive={isUserMenuActive}
+              />
+            )}
+          </Container.FlexRow>
+        </header>
+      )}
+      {!isOverSTabletBreakPoint &&
+        headerConfig.isGNBExistBottomUnderSTablet && (
+          <GNB
+            className={cn(
+              commonHeaderStyle,
+              'bottom-0 left-0 justify-center bg-bg',
+            )}
           />
         )}
-      </Container.FlexRow>
-    </header>
+    </>
   );
 }
 
--- src/components/templates/Layout.template.tsx
+++ src/components/templates/Layout.template.tsx
@@ -3,16 +3,12 @@ import { Outlet, useLocation } from 'react-router-dom';
 import cn from '@/libs/cn';
 import { useAuthState } from '@/hooks/useSign';
 import Header from '@/components/templates/Header';
-import { GNB } from '@/components/organisms/header';
 import isRoutePathMatched from '@/libs/isPathMatched';
-import { routeHeaderInfo, routePaths } from '@/constants/route';
-import useIsOverSTabletBreakpoint from '@/hooks/useIsOverSTabletBreakpoint';
 
 export default function LayoutTemplate() {
   // * supabase authListener를 등록함과 동시에 isLogin상태를 가져오기 위함
   const [session] = useAuthState();
   const location = useLocation();
-  const [isOverSTabletBreakPoint] = useIsOverSTabletBreakpoint();
   const isSignPath = isRoutePathMatched(location.pathname, [
     'sign',
     'signIn',
@@ -28,41 +24,12 @@ export default function LayoutTemplate() {
   ]);
   const commonHeaderStyle = 'flex h-[8rem] items-center fixed w-screen z-50';
 
-  const getHeaderConfig = (locationPathName: string) => {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const routePathType of Object.keys(routePaths)) {
-      if (
-        isRoutePathMatched(
-          locationPathName,
-          routePathType as keyof typeof routePaths,
-        )
-      )
-        return routeHeaderInfo[routePathType as keyof typeof routePaths];
-    }
-
-    return routeHeaderInfo.default;
-  };
-
-  const headerConfig = getHeaderConfig(location.pathname);
-
   return (
     <>
-      {(isOverSTabletBreakPoint || headerConfig.isHeaderExistOnMobile) && (
-        <Header
-          isLogin={!!session}
-          className={cn(commonHeaderStyle, isSignPath && 'bg-transparent')}
-          exist={headerConfig}
-        />
-      )}
-      {!isOverSTabletBreakPoint &&
-        headerConfig.isGNBExistBottomUnderSTablet && (
-          <GNB
-            className={cn(
-              commonHeaderStyle,
-              'bottom-0 left-0 justify-center bg-bg',
-            )}
-          />
-        )}
+      <Header
+        isLogin={!!session}
+        className={cn(commonHeaderStyle, isSignPath && 'bg-transparent')}
+      />
       <main
         className={cn(
           'flex flex-col relative max-w-[79rem] p-5 mx-auto h-screen',
