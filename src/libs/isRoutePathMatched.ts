74d049c8c1d48d318131d143c4a42c8d7556f40d
UHa <https00200@gmail.com>
2024-09-11 00:41:35 +0900

    🚑 [Fix]: Add custom type gard to resolve type errors
    - routePaths[routePath], routePaths[path]의 타입을 typescript가 제대로 인식하지 못해서 오류가 생김
    - typeof 체크만으로 타입을 좁히지 못해 사용자 정의 타입 가드(isFunction)을 만들어 수정

--- src/libs/isRoutePathMatched.ts
+++ src/libs/isRoutePathMatched.ts
@@ -2,30 +2,37 @@ import { matchPath } from 'react-router-dom';
 
 import { routePaths } from '@/constants/route';
 
+const isFunction = (route: unknown): route is () => string =>
+  typeof route === 'function';
+
 const isRoutePathMatched = (
   currentRoutePath: string,
   routePath: keyof typeof routePaths | (keyof typeof routePaths)[],
 ) => {
   if (typeof routePath === 'string') {
-    if (typeof routePaths[routePath] === 'function')
+    const path = routePaths[routePath];
+
+    if (isFunction(path)) {
       // * react router dom matchPath API는 path가 일치한다면 객체를 반환하고 일치하지 않는다면 null을 반환한다.
-      return !!matchPath(routePaths[routePath](), currentRoutePath);
-    if (typeof routePaths[routePath] === 'string')
-      return !!matchPath(routePaths[routePath], currentRoutePath);
+      return !!matchPath(path(), currentRoutePath);
+    }
+
+    if (typeof path === 'string') {
+      return !!matchPath(path, currentRoutePath);
+    }
   } else if (Array.isArray(routePath)) {
     let isMatched = false;
 
     // eslint-disable-next-line no-restricted-syntax
     for (const path of routePath) {
-      if (typeof routePaths[path] === 'function')
-        isMatched = isMatched || !!matchPath(routePaths[path](), currentRoutePath);
-      if (typeof routePaths[path] === 'string')
-        isMatched = isMatched || !!matchPath(routePaths[path], currentRoutePath);
+      const targetPath = routePaths[path];
+      if (isFunction(targetPath))
+        isMatched = isMatched || !!matchPath(targetPath(), currentRoutePath);
+      if (typeof targetPath === 'string')
+        isMatched = isMatched || !!matchPath(targetPath, currentRoutePath);
     }
-
-    return isMatched
+    return isMatched;
   }
-
   return false;
 };
 
