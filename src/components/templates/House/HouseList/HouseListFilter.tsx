77364480a7aa2d89bef6bbdad91ce268ebf3a5bf
rlwjd31 <rlwjd31@naver.com>
2024-10-07 17:25:23 +0900

    🎨 [style]: ReDesign HouseListFilter
    - shadow-badge를 공통 스타일로 추출하여 적용
    - 필요없는 style제거

--- src/components/templates/House/HouseList/HouseListFilter.tsx
+++ src/components/templates/House/HouseList/HouseListFilter.tsx
@@ -5,40 +5,32 @@ import IconButton from '@/components/molecules/IconButton';
 import cn from '@/libs/cn';
 
 export default function HouseListFilter() {
-  const filterBtnStyle =
-    'cursor-pointer items-center gap-3 rounded-full bg-white shadow-[0_3px_8px_0_rgba(0,0,0,16%)] text-brown hover:ring hover:ring-point active:bg-list-point active:text-white';
+  const filterBtnCommonStyle =
+    'cursor-pointer items-center gap-3 rounded-full bg-white shadow-badge text-brown hover:ring hover:ring-point active:bg-point1 active:text-white';
   return (
-    <Container.FlexCol className="mb-[3.75rem] gap-5">
-      <Container.FlexRow className="justify-center">
-        <Container.FlexRow className="gap-x-4">
+    <Container.FlexRow className="mb-[3.75rem] justify-center gap-5">
+      <Container.FlexRow className="gap-x-4">
+        <IconButton.Ghost
+          iconType="filter"
+          direction="left"
+          className={cn(filterBtnCommonStyle, 'px-[1.6rem] py-[1rem]')}
+        >
+          <Typography.SpanMid2 className="text-[1rem]">
+            필터
+          </Typography.SpanMid2>
+        </IconButton.Ghost>
+        <Link to="/house/regist">
           <IconButton.Ghost
-            iconType="filter"
+            iconType="add"
             direction="left"
-            className={cn(
-              'h-[2.6875rem] px-[0.9063rem] tablet:h-[3.9375rem] tablet:px-[1.5625rem]',
-              filterBtnStyle,
-            )}
+            className={cn(filterBtnCommonStyle, 'px-[1.6rem] py-[1rem] ')}
           >
-            <Typography.SpanMid2 className="leading-150 tablet:text-[1rem]">
-              필터
+            <Typography.SpanMid2 className="text-[1rem]">
+              하우스 등록
             </Typography.SpanMid2>
           </IconButton.Ghost>
-          <Link to="/house/regist">
-            <IconButton.Ghost
-              iconType="add"
-              direction="left"
-              className={cn(
-                'h-[2.6875rem] px-[0.9063rem] tablet:h-[3.9375rem] tablet:px-[1.5625rem]',
-                filterBtnStyle,
-              )}
-            >
-              <Typography.SpanMid2 className="leading-150 tablet:text-[1rem]">
-                하우스 등록
-              </Typography.SpanMid2>
-            </IconButton.Ghost>
-          </Link>
-        </Container.FlexRow>
+        </Link>
       </Container.FlexRow>
-    </Container.FlexCol>
+    </Container.FlexRow>
   );
 }
