c6106fc11e297106e2647208c38bcf83bfe4bac6
rlwjd31 <rlwjd31@naver.com>
2024-10-10 20:44:45 +0900

    🚑️ [Fix]: 버그 수정 및 단순 텍스트 수정
    sign/up/info는 sign/up의 nested router이기 때문에 SignLayoutWrapper로 감싸면
    두 번을 감싸는 격으로 layout이 망가져 다시 제거

--- src/components/pages/SignUpInfo.tsx
+++ src/components/pages/SignUpInfo.tsx
@@ -1,10 +1,5 @@
-import { SignLayoutWrapper } from '@/components/templates/SignLayout.template';
 import SignUpInfoTemplate from '@/components/templates/SignUpInfo.template';
 
 export default function SignUpInfo() {
-  return (
-    <SignLayoutWrapper className="gap-[2.5rem]">
-      <SignUpInfoTemplate />
-    </SignLayoutWrapper>
-  );
+  return <SignUpInfoTemplate />;
 }
